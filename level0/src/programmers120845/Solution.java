0ef3700407563e67bba70621966ba2f2e6346269
JIHYE <jdh324@naver.com>
2022-12-12 22:54:47 +0900

    no message

---
+++ level0/src/programmers120845/Solution.java
@@ -0,0 +1,15 @@
+package programmers120845;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] box = {10, 8, 6};
+        int answer = solution(box, 3);
+        System.out.println(answer);
+
+    }
+
+
+    public static int solution(int[] box, int n) {
+        return (box[0]/n) * (box[1]/n) * (box[2]/n);
+    }
+}
