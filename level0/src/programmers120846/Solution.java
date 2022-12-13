457bc2f4a5dedd52d40b6eb4f389958072f2acb3
JIHYE <jdh324@naver.com>
2022-12-13 14:10:00 +0900

    no message

---
+++ level0/src/programmers120846/Solution.java
@@ -0,0 +1,19 @@
+package programmers120846;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(solution(15));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int num = 0;
+            for (int j = 1; j <= i; j++) {
+                if(i % j == 0) num++;
+            }
+            if(num >= 3) answer++;
+        }
+        return answer;
+    }
+}
