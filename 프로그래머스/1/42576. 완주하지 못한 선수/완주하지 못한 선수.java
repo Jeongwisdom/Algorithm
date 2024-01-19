9d9a9162a15787ea6502c40ba44de9921154101a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-01-20 05:54:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.84 ms, Memory: 96.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String s : participant) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : completion) {
+            if (map.get(s) == 1) map.remove(s);
+            else map.put(s, map.get(s) - 1);
+        }
+
+        String answer = "";
+        for (String s: map.keySet()) {
+            answer = s;
+        }
+
+        return answer;
+    }
+}
