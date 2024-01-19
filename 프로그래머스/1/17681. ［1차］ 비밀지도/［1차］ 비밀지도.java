02d5f1d20e93a6ea7b2a161f0be79cc0e719947d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-01-19 09:38:13 +0900

    [level 1] Title: [1차] 비밀지도, Time: 9.73 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String[] ar1 = new String[n];
+        String[] ar2 = new String[n];
+        
+        for (int i = 0; i < n; i++) {
+            ar1[i] = String.format("%0" + n + "d", Long.parseLong(Integer.toBinaryString(arr1[i])));
+            ar2[i] = String.format("%0" + n + "d", Long.parseLong(Integer.toBinaryString(arr2[i])));
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                if (ar1[i].charAt(j) == '0' && ar2[i].charAt(j) == '0') {
+                    sb.append(" ");
+                } else sb.append("#");
+            }
+            answer[i] = sb.toString();
+        }
+        
+        return answer;
+    }
+}
