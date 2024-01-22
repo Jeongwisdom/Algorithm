d2b44db4861ce2d89ab20c5e2da121ec902ae18e
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-01-23 06:22:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.12 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -4,8 +4,13 @@ class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         int[] answer = new int[id_list.length];
         Map<String, Set<String>> map = new HashMap<>();
+        Map<String, Integer> id = new HashMap<>();
         Set<String> set;
         String[] reporting;
+        
+        for (int i = 0; i < id_list.length; i++) {
+            id.put(id_list[i], i);
+        }
         
         for (String s: report) {
             reporting = s.split(" ");
@@ -17,12 +22,7 @@ class Solution {
         for (Set<String> reportee: map.values()) {
             if (reportee.size() >= k) {
                 for (String reporter: reportee) {
-                    for (int i = 0; i < id_list.length; i++) {
-                        if (id_list[i].equals(reporter)) {
-                            answer[i]++;
-                            break;
-                        }
-                    }
+                    answer[id.get(reporter)]++;
                 }
             }
         }
