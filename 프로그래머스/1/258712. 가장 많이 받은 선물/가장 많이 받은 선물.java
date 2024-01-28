bcd142441b117fc71befaa36d899c4d6727caa7b
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-01-28 09:19:27 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 3.80 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[] friends, String[] gifts) {
+        Map<String, Present> map = new HashMap<>();
+        int length = friends.length;
+        for (int i = 0; i < length; i++) {
+            map.put(friends[i], new Present(i, length));
+        }
+        
+        String[] s;
+        Present a, b;
+        for (String gift: gifts) {
+            s = gift.split(" ");
+            a = map.get(s[0]);
+            b = map.get(s[1]);
+            a.give[b.idx]++;
+            b.get[a.idx]++;
+        }
+        
+        for (String friend: friends) {
+            a = map.get(friend);
+            a.calculate();
+        }
+        
+        int[] next = new int[friends.length];
+        for (int i = 0; i < friends.length - 1; i++) {
+            a = map.get(friends[i]);
+            for (int j = i + 1; j < friends.length; j++) {
+                b = map.get(friends[j]);
+                if (a.give[b.idx] > b.give[a.idx]) next[a.idx]++;
+                else if (a.give[b.idx] < b.give[a.idx]) next[b.idx]++;
+                else {
+                    if (a.present > b.present) next[a.idx]++;
+                    else if (a.present < b.present) next[b.idx]++;
+                }
+            }
+        }
+        
+        int answer = 0;
+        for (int n: next) {
+            answer = Math.max(answer, n);
+        }
+        
+        return answer;
+    }
+}
+
+class Present {
+    
+    int idx;
+    int[] give;
+    int[] get;
+    int present;
+    
+    public Present(int idx, int length) {
+        this.idx = idx;
+        this.give = new int[length];
+        this.get = new int[length];
+    }
+    
+    public void calculate() {
+        int give = 0;
+        int get = 0;
+        for (int i = 0; i < this.get.length; i++) {
+            give += this.give[i];
+            get += this.get[i];
+        }
+        
+        this.present = give - get;
+    }
+}
