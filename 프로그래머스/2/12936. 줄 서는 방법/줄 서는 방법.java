9ae449f97ca630f787d95096e00fed12475a659a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-04 03:32:21 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -1,28 +1,23 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int n, long k) {
         int[] answer = new int[n];
-        boolean[] ch = new boolean[n + 1];
+        List<Integer> list = new ArrayList<>();
         long[] fac = factorial(n);
         
-        int id, count;
+        for (int i = 1; i <= n; i++) {
+            list.add(i);
+        }
+        
+        int id;
         for (int i = 0; i < answer.length; i++) {
             id = (int) (k / fac[n - (i + 1)]);
             k = (long) (k % fac[n - (i + 1)]);
             if (k == 0) {
                 k = (long) fac[n - (i + 1)];
             } else id++;
-            count = 0;
-            for (int j = 1; j < ch.length; j++) {
-                if (!ch[j]) {
-                    count++;
-                    if (id == count) {
-                        id = j;
-                        break;
-                    }
-                }
-            }
-            ch[id] = true;
-            answer[i] = id;
+            answer[i] = list.remove(id - 1);
         }
         
         return answer;
