bec8e490dad6db3a53805c87dbc40065ebff8dd1
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-06 22:32:11 +0900

    [Bronze V] Title: 구구단, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i <= 9; i++) {
+            sb.append(n)
+                .append(" * ")
+                .append(i)
+                .append(" = ")
+                .append(n * i)
+                .append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
