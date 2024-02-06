021b68b035e3fc0665d6aacc4a46e94af75fe2e6
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-06 18:48:52 +0900

    [Bronze IV] Title: 주사위 세개, Time: 120 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -5,25 +5,15 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<Integer, Integer> map = new HashMap<>();
-        int a;
-        for (int i = 0; i < 3; i++) {
-            a = Integer.parseInt(st.nextToken());
-            map.put(a, map.getOrDefault(a, 0) + 1);
-        }
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
 
-        int sum = 0;
-        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
-            if (entry.getValue() == 3) {
-                sum = entry.getKey() * 1000 + 10000;
-                break;
-            } else if (entry.getValue() == 2) {
-                sum = entry.getKey() * 100 + 1000;
-                break;
-            } else {
-                sum = Math.max(sum, entry.getKey() * 100);
-            }
-        }
-        System.out.print(sum);
+        int answer = 0;
+        if (a == b && b == c) answer = a * 1000 + 10000;
+        else if (a == b || a == c) answer = a * 100 + 1000;
+        else if (b == c) answer = b * 100 + 1000;
+        else answer = Math.max(a, Math.max(b, c)) * 100;
+        System.out.print(answer);
     }
 }
