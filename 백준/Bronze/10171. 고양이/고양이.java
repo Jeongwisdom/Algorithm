fba7ef8b6c43d405d62c8a0732a2b4cfd9b9dcba
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-06 17:35:45 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13896 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\\n");
+        sb.append(" )  ( ')\n");
+        sb.append("(  /  )\n");
+        sb.append(" \\(__)|");
+        System.out.println(sb);
+    }
+}
