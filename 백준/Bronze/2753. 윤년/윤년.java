01c12cf62fa77455a20a178689281c17efeb6f8c
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-06 17:58:41 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -4,7 +4,7 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int year = Integer.parseInt(br.readLine());
-        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) System.out.print(1);
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) System.out.print(1);
         else System.out.print(0);
     }
 }
