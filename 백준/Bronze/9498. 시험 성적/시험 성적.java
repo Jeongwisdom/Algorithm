4e0d42a24ea2208feaf9dbd4b9ff8f55f03444d8
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-06 17:52:05 +0900

    [Bronze V] Title: 시험 성적, Time: 120 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -4,10 +4,10 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int score = Integer.parseInt(br.readLine());
-        if (score >= 90 && score <= 100) System.out.print("A");
-        else if (score >= 80 && score <= 89) System.out.print("B");
-        else if (score >= 70 && score <= 79) System.out.print("C");
-        else if (score >= 60 && score <= 69) System.out.print("D");
+        if (score >= 90) System.out.print("A");
+        else if (score >= 80) System.out.print("B");
+        else if (score >= 70) System.out.print("C");
+        else if (score >= 60) System.out.print("D");
         else System.out.print("F");
     }
 }
