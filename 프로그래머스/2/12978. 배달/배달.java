6dfd1d5e9cfd2f8676ed718f68e2a60f422c6744
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-12 04:23:46 +0900

    [level 2] Title: 배달, Time: 1.02 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,13 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        List<Map<Integer, Integer>> list = new ArrayList<>();
-        for (int i = 0; i < N + 1; i++) {
-            list.add(new HashMap<>());
-        }
+        int[][] arr = new int[N + 1][N + 1];
         for (int i = 0; i < road.length; i++) {
-            list.get(road[i][0]).put(road[i][1], Math.min(list.get(road[i][0]).getOrDefault(road[i][1], Integer.MAX_VALUE), road[i][2]));
-            list.get(road[i][1]).put(road[i][0], Math.min(list.get(road[i][1]).getOrDefault(road[i][0], Integer.MAX_VALUE), road[i][2]));
+            arr[road[i][0]][road[i][1]] = arr[road[i][0]][road[i][1]] == 0? road[i][2]: Math.min(arr[road[i][0]][road[i][1]], road[i][2]);
+            arr[road[i][1]][road[i][0]] = arr[road[i][1]][road[i][0]] == 0? road[i][2]: Math.min(arr[road[i][1]][road[i][0]], road[i][2]);
         }
         
         int[] dis = new int[N + 1];
@@ -17,16 +14,14 @@ class Solution {
         }
         Queue<int[]> queue = new PriorityQueue<>((q1, q2) -> q1[1] - q2[1]);
         queue.offer(new int[] {1, 0});
-        int[] poll;
-        Map<Integer, Integer> map;
+        int[] n;
         while (!queue.isEmpty()) {
-            poll = queue.poll();
-            if (dis[poll[0]] >= poll[1]) {
-                map = list.get(poll[0]);
-                for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
-                    if (dis[entry.getKey()] > dis[poll[0]] + entry.getValue()) {
-                        dis[entry.getKey()] = dis[poll[0]] + entry.getValue();
-                        queue.offer(new int[] {entry.getKey(), entry.getValue()});
+            n = queue.poll();
+            if (dis[n[0]] >= n[1]) {
+                for (int i = 2; i < arr.length; i++) {
+                    if (arr[n[0]][i] != 0 && dis[i] > dis[n[0]] + arr[n[0]][i]) {
+                        dis[i] = dis[n[0]] + arr[n[0]][i];
+                        queue.offer(new int[] {i, arr[n[0]][i]});
                     }
                 }
             }
