e229391b26cf52727de916ffed53b6538773f1d4
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-13 03:32:49 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 8.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -1,54 +1,44 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] files) {
-        File[] file = new File[files.length];
-        for (int i = 0; i < files.length; i++) {
-            file[i] = new File(files[i]);
+        Arrays.sort(files, (s1, s2) -> {
+            int id1 = findDigit(s1);
+            int id2 = findDigit(s2);
+            String head1 = s1.substring(0, id1).toUpperCase();
+            String head2 = s2.substring(0, id2).toUpperCase();
+            int id3 = findNoDigit(id1, s1);
+            int id4 = findNoDigit(id2, s2);
+            int num1 = Integer.parseInt(s1.substring(id1, id3));
+            int num2 = Integer.parseInt(s2.substring(id2, id4));
+            if (head1.equals(head2)) return num1 - num2;
+            return head1.compareTo(head2);
+        });
+        
+        return files;
+    }
+    
+    public int findDigit(String s) {
+        int id = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                id = i;
+                break;
+            }
         }
         
-        Arrays.sort(file);
-        String[] answer = new String[files.length];
-        for (int i = 0; i < file.length; i++) {
-            answer[i] = file[i].file;
-        }
-        return answer;
-    }
-}
-
-class File implements Comparable<File> {
-    
-    String file;
-    String head;
-    int num;
-    
-    public File(String file) {
-        this.file = file;
-        int id1 = 0;
-        for (int i = 0; i < file.length(); i++) {
-            if (Character.isDigit(file.charAt(i))) {
-                id1 = i;
-                break;
-            }
-        }
-        int id2 = 0;
-        for (int i = id1 + 1; i <= id1 + 5; i++) {
-            if (i == file.length()) {
-                id2 = i;
-                break;
-            }
-            if (!Character.isDigit(file.charAt(i))) {
-                id2 = i;
-                break;
-            }
-        }
-        this.head = file.substring(0, id1).toUpperCase();
-        this.num = Integer.parseInt(file.substring(id1, id2));
+        return id;
     }
     
-    @Override
-    public int compareTo(File f) {
-        if (this.head.equals(f.head)) return this.num - f.num;
-        return this.head.compareTo(f.head);
+    public int findNoDigit(int id, String s) {
+        int idx = id + 5;
+        for (int i = id + 1; i < id + 5; i++) {
+            if (i == s.length() || !Character.isDigit(s.charAt(i))) {
+                idx = i;
+                break;
+            }
+        }
+        
+        return idx;
     }
 }
