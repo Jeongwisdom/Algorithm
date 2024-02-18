900777142704e3009e8b6b297ea2f281cbdbdb97
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-18 18:08:34 +0900

    [level 2] Title: 오픈채팅방, Time: 193.23 ms, Memory: 147 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -3,28 +3,24 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] record) {
         Map<String, String> map = new HashMap<>();
-        int num = 0;
+        List<String[]> list = new ArrayList<>();
         String[] tmp;
         for (int i = 0; i < record.length; i++) {
             tmp = record[i].split(" ");
-            if (tmp[0].equals("Enter")) {
+            if (tmp[0].charAt(0) == 'E') {
                 map.put(tmp[1], tmp[2]);
-                num++;
-            } else if (tmp[0].equals("Change")) {
+                list.add(new String[] {tmp[1], "님이 들어왔습니다."});
+            } else if (tmp[0].charAt(0) == 'C') {
                 map.put(tmp[1], tmp[2]);
             } else {
-                num++;
+                list.add(new String[] {tmp[1], "님이 나갔습니다."});
             }
         }
         
-        String[] answer = new String[num];
-        for (int i = 0, j = 0; i < record.length; i++) {
-            tmp = record[i].split(" ");
-            if (tmp[0].equals("Enter")) {
-                answer[j++] = map.get(tmp[1]) + "님이 들어왔습니다.";
-            } else if (tmp[0].equals("Leave")) {
-                answer[j++] = map.get(tmp[1]) + "님이 나갔습니다.";
-            }
+        String[] answer = new String[list.size()];
+        for (int i = 0, j = 0; i < list.size(); i++) {
+            tmp = list.get(i);
+            answer[i] = map.get(tmp[0]) + tmp[1];
         }
         
         return answer;
