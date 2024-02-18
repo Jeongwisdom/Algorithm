4b6dd09302bb506abb43e401e417cd8be5a0b9ac
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-02-18 14:55:02 +0900

    [level 2] Title: 스킬트리, Time: 0.54 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,19 +1,11 @@
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        boolean ch;
+        StringBuilder match = new StringBuilder();
+        match.append("[^").append(skill).append("]");
         for (String str: skill_trees) {
-            ch = true;
-            for (int i = 0, j = 0; i < str.length(); i++) {
-                if (skill.contains(String.valueOf(str.charAt(i)))) {
-                    if (str.charAt(i) == skill.charAt(j)) j++;
-                    else {
-                        ch = false;
-                        break;
-                    }
-                }
-            }
-            if (ch) answer++;
+            str = str.replaceAll(match.toString(), "");
+            if (skill.startsWith(str)) answer++;
         }
 
         return answer;
