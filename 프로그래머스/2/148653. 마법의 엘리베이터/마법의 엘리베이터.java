71424a0f610d92749cc25720c6d65e7a670a533a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-02 05:36:50 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.01 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int storey) {
+        int answer = 0;
+        while (storey != 0) {
+            int remain = storey % 10;
+            storey = storey / 10;
+            if (remain > 5 || (remain == 5 && storey % 10 >= 5)) {
+                storey++;
+                answer += 10 - remain;
+            } else {
+                answer += remain; 
+            }
+        }
+        
+        return answer;
+    }
+}
