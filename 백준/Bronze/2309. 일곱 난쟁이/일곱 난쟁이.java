3b54d8c82b0c1fa309bd62bcfbd0d37bfee34569
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-05 00:04:25 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -5,27 +5,29 @@ class Main {
     static boolean flag = false;
     static boolean[] ch = new boolean[9];
     static int[] arr = new int[9];
-    static int[] answer = new int[7];
+    static int[] answer = new int[2];
+    static int total = 0;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int i = 0; i < 9; i++) {
             arr[i] = Integer.parseInt(br.readLine());
+            total += arr[i];
         }
+        
         Arrays.sort(arr);
         DFS(0, 0, new int[7]);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < answer.length; i++) {
-            sb.append(answer[i]);
-            if (i < answer.length - 1) sb.append("\n");
+        for (int i = 0; i < arr.length; i++) {
+            if (i != answer[0] && i != answer[1]) sb.append(arr[i]).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     } 
     
     public static void DFS(int count, int sum, int[] a) {
         if (!flag) {
-            if (count == answer.length) {
-                if (sum == 100) {
+            if (count == 2) {
+                if (total - sum == 100) {
                     answer = a.clone();
                     flag = true;
                 }
@@ -33,7 +35,7 @@ class Main {
                 for (int i = count; i < arr.length; i++) {
                     if (!ch[i]) {
                         ch[i] = true;
-                        a[count] = arr[i];
+                        a[count] = i;
                         DFS(count + 1, sum + arr[i], a);
                         ch[i] = false;
                     }
