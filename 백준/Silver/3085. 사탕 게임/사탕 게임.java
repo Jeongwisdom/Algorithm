2314f9f8e1a6cdae452129c28e1683fad28e05c6
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-05 19:31:24 +0900

    [Silver II] Title: 사탕 게임, Time: 132 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -15,22 +15,34 @@ class Main {
         }
         
         for (int i = 0; i < n; i++) {
+            int rowSum = 1;
+            int colSum = 1;
             for (int j = 0; j < n - 1; j++) {
                 if (arr[i][j] != arr[i][j + 1]) {
                     char c = arr[i][j];
                     arr[i][j] = arr[i][j + 1];
                     arr[i][j + 1] = c;
-                    count(arr);
+                    count(arr, i , j);
+                    count(arr, i, j + 1);
                     arr[i][j + 1] = arr[i][j];
                     arr[i][j] = c;
+                    rowSum = 1;
+                } else {
+                    rowSum++;
+                    answer = Math.max(answer, rowSum);
                 }
                 if (arr[j][i] != arr[j + 1][i]) {
                     char c = arr[j][i];
                     arr[j][i] = arr[j + 1][i];
                     arr[j + 1][i] = c;
-                    count(arr);
+                    count(arr, j, i);
+                    count(arr, j + 1, i);
                     arr[j + 1][i] = arr[j][i];
                     arr[j][i] = c;
+                    colSum = 1;
+                } else {
+                    colSum++;
+                    answer = Math.max(answer, colSum);
                 }
             }
         }
@@ -38,18 +50,29 @@ class Main {
         System.out.println(answer);
     }
     
-    public static void count(char[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int rowSum = 1;
-            int colSum = 1;
-            for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[i][j] == arr[i][j + 1]) rowSum++;
-                else rowSum = 1;
-                if (arr[j][i] == arr[j + 1][i]) colSum++;
-                else colSum = 1;
-                answer = Math.max(answer, rowSum);
-                answer = Math.max(answer, colSum);
+    public static void count(char[][] arr, int x, int y) {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        for (int i = 0; i < dx.length; i++) {
+            char c = arr[x][y];
+            int sum = 0;
+            int nx = x;
+            int ny = y;
+            while (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr.length) {
+                if (c == arr[nx][ny]) sum++;
+                else break;
+                nx = nx + dx[i];
+                ny = ny + dy[i]; 
             }
+            nx = x - dx[i];
+            ny = y - dy[i];
+            while (nx >= 0 && ny >= 0 && nx < arr.length && ny < arr.length) {
+                if (c == arr[nx][ny]) sum++;
+                else break;
+                nx = nx - dx[i];
+                ny = ny - dy[i]; 
+            }
+            answer = Math.max(answer, sum);
         }
     }
 }
