8736aadfffe9d0e83980339d8a6f6bf001706f55
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-07 04:33:40 +0900

    [Bronze I] Title: 유레카 이론, Time: 140 ms, Memory: 14464 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -2,34 +2,30 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        int[] arr = new int[t];
-        for (int i = 0; i < t; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        boolean[] check = new boolean[1001];
+        int[] tri = new int[45];
+        tri[0] = 1;
+        for (int i = 1; i < 45; i++) {
+            tri[i] = tri[i - 1] + (i + 1);
         }
         
+        for (int i = 0; i < tri.length; i++) {
+            for (int j = 0; j < tri.length; j++) {
+                for (int k = 0; k < tri.length; k++) {
+                    int sum = tri[i] + tri[j] + tri[k];
+                    if (sum <= 1000) check[sum] = true;
+                }
+            }
+        }
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
-            if (check(arr[i], 0, 0)) bw.write("1");
+            if (check[Integer.parseInt(br.readLine())]) bw.write("1");
             else bw.write("0");
             bw.write("\n");
         }
         bw.flush();
-    }
-    
-    public static boolean check(int num, int count, int sum) {
-        if (count == 3) {
-            if (sum == num) return true;
-        } else {
-            int tri = 0;
-            for (int i = 1; tri < num; i++) {
-                tri += i;
-                boolean ch = check(num, count + 1, sum + tri);
-                if (ch) return true;
-            }
-        }
-        
-        return false;
     }
 }
