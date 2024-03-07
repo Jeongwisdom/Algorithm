82616846fbbf55fddbbf1839a6ac4b552428552b
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-07 20:19:25 +0900

    [Silver III] Title: 숫자 야구, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.java
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.java
@@ -6,38 +6,45 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        String[][] arr = new String[n][3];
+        int[][] arr = new int[n][3];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int j = 0;
             while (st.hasMoreTokens()) {
-                arr[i][j++] = st.nextToken();
+                arr[i][j++] = Integer.parseInt(st.nextToken());
             }
         }
         System.out.println(check(arr));
     }
     
-    public static int check(String[][] num) {
+    public static int check(int[][] num) {
         int answer = 0;
+        int[] arr = new int[3];
         for (int i = 1; i <= 9; i++) {
-            String s = String.valueOf(i);
+            arr[0] = i;
             for (int j = 1; j <= 9; j++) {
                 if (j == i) continue;
-                String st = s + j;
+                arr[1] = j;
                 loop:
                 for (int k = 1; k <= 9; k++) {
                     if (k == i || k == j) continue;
-                    String str = st + k;
-                    for (String[] strings : num) {
+                    arr[2] = k;
+                    for (int[] strings : num) {
+                        String str = String.valueOf(strings[0]);
                         int strike = 0;
                         int ball = 0;
                         for (int m = 0; m < 3; m++) {
-                            if (strings[0].charAt(m) == str.charAt(m))
+                            if (str.charAt(m) - '0' == arr[m])
                                 strike++;
-                            else if (str.contains(String.valueOf(strings[0].charAt(m))))
-                                ball++;
+                            else {
+                                for (int n = 0; n < 3; n++) {
+                                    if (m != n && str.charAt(n) - '0' == arr[m]) {
+                                        ball++;
+                                    }
+                                }
+                            }
                         }
-                        if (strike != Integer.parseInt(strings[1]) || ball != Integer.parseInt(strings[2]))
+                        if (strike != strings[1] || ball != strings[2])
                             continue loop;
                     }
                     answer++;
