a774b01e6669f5968d722f24842060c5b10bcdf3
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-08 16:48:41 +0900

    [Silver II] Title: 로또, Time: 136 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 class Main {
     static StringBuilder answer = new StringBuilder();
+    static int[] temp = new int[6];
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,20 +15,22 @@ class Main {
             for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
-            if (j != 0) answer.append("\n");
-            BF(arr, 0, new StringBuilder(), 0);
+            BF(arr, 0, 0);
+            answer.append("\n");
         }
         System.out.print(answer);
     }
     
-    public static void BF(int[] arr, int count, StringBuilder sb, int id) {
+    public static void BF(int[] arr, int count, int id) {
         if (count == 6) {
-            answer.append(sb).append("\n");
+            for (int num: temp) {
+                answer.append(num).append(" ");
+            }
+            answer.append("\n");
         } else {
             for (int i = id; i <= arr.length - 6 + count; i++) {
-                StringBuilder s = new StringBuilder(sb);
-                s.append(arr[i]).append(" ");
-                BF(arr, count + 1, s, i + 1);
+                temp[count] = arr[i];
+                BF(arr, count + 1, i + 1);
             }
         }
     }
