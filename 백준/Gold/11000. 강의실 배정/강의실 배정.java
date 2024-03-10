4189835fabea76695a554afb2ba9ed186539b45f
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-11 05:57:38 +0900

    [Gold V] Title: 강의실 배정, Time: 624 ms, Memory: 73640 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -6,23 +6,21 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][2];
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
         
         quickSort(arr, 0, n - 1);
         Queue<Integer> pq = new PriorityQueue<>();
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            while (!pq.isEmpty() && pq.peek() <= arr[i][0]) {
-                pq.poll();
-            }
+        pq.offer(arr[0][1]);
+        for (int i = 1; i < n; i++) {
+            if (pq.peek() <= arr[i][0]) pq.poll();
             pq.offer(arr[i][1]);
-            answer = Math.max(answer, pq.size());
         }
-        System.out.println(answer);
+        System.out.println(pq.size());
     }
     
     public static void quickSort(int[][] arr, int l, int r) {
