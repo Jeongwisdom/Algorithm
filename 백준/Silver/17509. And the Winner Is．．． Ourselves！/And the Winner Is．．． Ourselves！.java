3686fec91eca66bafdb8bae0e73c736266f68268
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-11 01:16:09 +0900

    [Silver V] Title: And the Winner Is... Ourselves!, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/17509. And the Winner Is．．． Ourselves！/And the Winner Is．．． Ourselves！.java
+++ 백준/Silver/17509. And the Winner Is．．． Ourselves！/And the Winner Is．．． Ourselves！.java
@@ -4,19 +4,20 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[11];
+        int[] arr = new int[291];
         int answer = 0;
         for (int i = 0; i < 11; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+            arr[Integer.parseInt(st.nextToken())]++;
             answer += 20 * Integer.parseInt(st.nextToken());
         }
         
-        Arrays.sort(arr);
-        answer += arr[0];
-        for (int i = 1; i < 11; i++) {
-            arr[i] += arr[i - 1];
-            answer += arr[i];
+        int last = 0;
+        for (int i = 1; i < 291; i++) {
+            if (arr[i] != 0) {
+                answer += last * arr[i] + i * arr[i] * (arr[i] + 1) / 2;
+                last = last + i * arr[i];
+            }
         }
         
         System.out.println(answer);
