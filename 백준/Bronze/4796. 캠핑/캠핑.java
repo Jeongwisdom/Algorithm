43a2610324dba60b61993075a0bd2c28ec6f8212
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-10 22:19:08 +0900

    [Bronze I] Title: 캠핑, Time: 132 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Bronze/4796. 캠핑/캠핑.java
+++ 백준/Bronze/4796. 캠핑/캠핑.java
@@ -4,7 +4,7 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; ; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int l = Integer.parseInt(st.nextToken());
@@ -13,12 +13,8 @@ class Main {
             if (l == 0 && p == 0 && v == 0) break;
             int answer = v / p * l;
             answer += v % p > l? l: v % p;
-            bw.write("Case ");
-            bw.write(String.valueOf(i));
-            bw.write(": ");
-            bw.write(String.valueOf(answer));
-            bw.write("\n");
+            sb.append("Case ").append(i).append(": ").append(answer).append("\n");
         }
-        bw.flush();
+        System.out.println(sb);
     }
 }
