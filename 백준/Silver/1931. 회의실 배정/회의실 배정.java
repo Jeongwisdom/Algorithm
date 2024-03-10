d03e90359a54d56b47026380ad08257b1a00793b
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-11 04:15:12 +0900

    [Silver I] Title: 회의실 배정, Time: 516 ms, Memory: 43960 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -8,9 +8,8 @@ class Main {
         int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
         
         int answer = 0;
