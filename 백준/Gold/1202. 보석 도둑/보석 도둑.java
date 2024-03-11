00b9f9514c81d7cd4a91f829713c348f6aa5e9b6
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-11 20:18:32 +0900

    [Gold II] Title: 보석 도둑, Time: 1380 ms, Memory: 130736 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -7,11 +7,13 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] crystal = new int[n][2];
+        Queue<int[]> q = new PriorityQueue<>((c1, c2) -> {
+            if (c2[1] == c1[1]) return c1[0] - c2[0];
+            return c2[1] - c1[1];
+        });
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            crystal[i][0] = Integer.parseInt(st.nextToken());
-            crystal[i][1] = Integer.parseInt(st.nextToken());
+            q.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
         TreeMap<Integer, Integer> bag = new TreeMap<>();
         for (int i = 0; i < k; i++) {
@@ -19,21 +21,16 @@ class Main {
             bag.put(m, bag.getOrDefault(m, 0) + 1);
         }
         
-        Arrays.sort(crystal, (c1, c2) -> {
-            if (c2[1] == c1[1]) return c1[0] - c2[0];
-            return c2[1] - c1[1];
-        });
         long answer = 0;
-        int i = 0;
-        while (k > 0 && i < n) {
-            Integer key = bag.ceilingKey(crystal[i][0]);
+        while (!q.isEmpty() && k > 0) {
+            int[] crystal = q.poll();
+            Integer key = bag.ceilingKey(crystal[0]);
             if (key != null) {
                 if (bag.get(key) > 1) bag.put(key, bag.get(key) - 1);
                 else bag.remove(key);
-                answer += crystal[i][1];
+                answer += crystal[1];
                 k--;
             }
-            i++;
         }
         System.out.println(answer);
     }
