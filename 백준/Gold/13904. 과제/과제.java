ce221c69bb367c780e914a81b55ef44e4f6f0fe3
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-12 01:05:07 +0900

    [Gold III] Title: 과제, Time: 144 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -6,8 +6,8 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         Queue<int[]> pq = new PriorityQueue<>((q1, q2) -> {
-            if(q1[0] == q2[0]) return q2[1] - q1[1];
-            return q1[0] - q2[0];
+            if(q2[1] == q1[1]) return q1[0] - q2[0];
+            return q2[1] - q1[1];
         });
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,17 +16,14 @@ class Main {
         
         int day = 0;
         int answer = 0;
-        Queue<int[]> q = new PriorityQueue<>((q1, q2) -> q1[1] - q2[1]);
+        int[] limit = new int[1001];
         while (!pq.isEmpty()) {
             int[] report = pq.poll();
-            if (day < report[0]) {
-                q.offer(report);
-                answer += report[1];
-                day++;
-            } else {
-                if (report[1] > q.peek()[1]) {
-                    answer += report[1] - q.poll()[1];
-                    q.offer(report);
+            for (int i = report[0]; i > 0; i--) {
+                if (limit[i] == 0) {
+                    limit[i] = 1;
+                    answer += report[1];
+                    break;
                 }
             }
         }
