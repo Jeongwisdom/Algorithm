131cd2b850d4406d8f84c860792b1bd52ba209d7
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-12 01:44:20 +0900

    [Gold V] Title: Rest Stops, Time: 516 ms, Memory: 44192 KB -BaekjoonHub

---
+++ 백준/Gold/15748. Rest Stops/Rest Stops.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int l = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int[][] rest = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            rest[i][0] = Integer.parseInt(st.nextToken());
+            rest[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(rest, (r1, r2) -> {
+            if (r2[1] == r1[1]) return r1[0] - r2[0];
+            return r2[1] - r1[1];
+        });
+        long diff = f - b;
+        int last = 0;
+        long answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (last < rest[i][0]) {
+                answer += (rest[i][0] - last) * diff * rest[i][1];
+                last = rest[i][0];
+            }
+        }
+        System.out.println(answer);
+    }
+}
