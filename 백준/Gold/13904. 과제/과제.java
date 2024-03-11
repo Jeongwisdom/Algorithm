d1020bb857c4498dbed54bbf0b74765912488179
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-12 01:08:18 +0900

    [Gold III] Title: 과제, Time: 144 ms, Memory: 14508 KB -BaekjoonHub

--- 백준/Gold/13904. 과제/과제.java
+++ 백준/Gold/13904. 과제/과제.java
@@ -5,23 +5,24 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Queue<int[]> pq = new PriorityQueue<>((q1, q2) -> {
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr, (q1, q2) -> {
             if(q2[1] == q1[1]) return q1[0] - q2[0];
             return q2[1] - q1[1];
         });
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            pq.offer(new int[] {Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
-        }
-        
         int answer = 0;
         boolean[] limit = new boolean[1001];
-        while (!pq.isEmpty()) {
-            int[] report = pq.poll();
-            for (int i = report[0]; i > 0; i--) {
-                if (!limit[i]) {
-                    limit[i] = true;
-                    answer += report[1];
+        for (int i = 0; i < n; i++) {
+            for (int j = arr[i][0]; j > 0; j--) {
+                if (!limit[j]) {
+                    limit[j] = true;
+                    answer += arr[i][1];
                     break;
                 }
             }
