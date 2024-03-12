495cb09800c760aac7ce0c12b07fc02702be81ca
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-12 13:40:54 +0900

    [Gold V] Title: 센서, Time: 180 ms, Memory: 16584 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(arr);
+        int[] diff = new int[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            diff[i] = arr[i + 1] - arr[i];
+        }
+        
+        Arrays.sort(diff);
+        int answer = 0;
+        for (int i = 0; i < n - k; i++) {
+            answer += diff[i];
+        }
+        System.out.println(answer);
+    }
+}
