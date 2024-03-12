6f36884cbfb623c367f8df88788a2a3bebde0f2f
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-12 20:00:30 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -2,8 +2,6 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    static int answer = 0;
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,18 +9,17 @@ class Main {
         int r = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
         
-        divide((int) Math.pow(2, n) / 2, r, c);
+        int half = (int) Math.pow(2, n) / 2;
+        int answer = 0;
+        while (half > 0) {
+            int row = 0, col = 0;
+            if (r >= half) row += 2;
+            if (c >= half) col += 1;
+            answer += half * half * (row + col);
+            r -= half * row / 2;
+            c -= half * col;
+            half /= 2;
+        }
         System.out.println(answer);
-    }
-    
-    public static void divide(int n, int r, int c) {
-        if (n == 0) return;
-        int row = 0;
-        int col = 0;
-        if (r >= n) row += 2;
-        if (c >= n) col += 1;
-        
-        answer += n * n * (row + col);
-        divide(n / 2, r - n * row / 2, c - n * col);
     }
 }
