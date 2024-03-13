4d1b1867df06fbc402096f1c9160a68c4ff4d9ce
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-13 20:17:48 +0900

    [Silver IV] Title: 수 찾기, Time: 620 ms, Memory: 47344 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,56 +5,23 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[][] arr = new int[2][];
-        for (int i = 0; i < 2; i++) {
-            int n = Integer.parseInt(br.readLine());
-            arr[i] = new int[n];
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+        Set<Integer> set = new HashSet<>();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+        int m = Integer.parseInt(br.readLine());
+        int[] arr = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        quickSort(arr[0], 0, arr[0].length - 1);
-        for (int i = 0; i < arr[1].length; i++) {
-            if (check(arr[0], arr[1][i])) bw.write("1\n");
+        for (int i = 0; i < m; i++) {
+            if (set.contains(arr[i])) bw.write("1\n");
             else bw.write("0\n");
         }
         bw.flush();
-    }
-
-    public static boolean check(int[] arr, int num) {
-        int l = 0, r = arr.length - 1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (num == arr[m]) return true;
-            else if (num < arr[m]) r = m - 1;
-            else if (num > arr[m]) l = m + 1;
-        }
-        return false;
-    }
-
-    public static void quickSort(int[] arr, int l, int r) {
-        int part = partition(arr, l, r);
-        if (l < part - 1) quickSort(arr, l, part - 1);
-        if (r > part) quickSort(arr, part, r);
-    }
-
-    public static int partition(int[] arr, int l, int r) {
-        int m = (l + r) / 2;
-        int pivot = arr[m];
-
-        while (l <= r) {
-            while (arr[l] < pivot) l++;
-            while (arr[r] > pivot) r--;
-            if (l <= r) {
-                int tmp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = tmp;
-                l++;
-                r--;
-            }
-        }
-        return l;
     }
 }
