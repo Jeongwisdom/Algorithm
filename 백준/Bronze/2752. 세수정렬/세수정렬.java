12e23b1d5aeace92331027391c350b4de2042ddd
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-03-27 00:57:44 +0900

    [Bronze IV] Title: 세수정렬, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2752. 세수정렬/세수정렬.java
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -9,11 +9,34 @@ class Main {
         for (int i = 0; i < 3; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        quickSort(arr, 0, 2);
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 3; i++) {
             sb.append(arr[i]).append("\n");
         }
         System.out.println(sb);
+    }
+    
+    public static void quickSort(int[] arr, int l, int r) {
+        int part = partition(arr, l, r);
+        if (l < part - 1) quickSort(arr, l, part - 1);
+        if (r > part) quickSort(arr, part, r);
+    }
+    
+    public static int partition(int[] arr, int l, int r) {
+        int m = (l + r) / 2;
+        int mid = arr[m];
+        while (l <= r) {
+            while (arr[l] < mid) l++;
+            while (mid < arr[r]) r--;
+            if (l <= r) {
+                int tmp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+        return l;
     }
 }
