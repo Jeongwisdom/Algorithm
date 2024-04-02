415ab683b028152daedc3f7811ba7e57535924d0
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-02 16:57:56 +0900

    [Bronze II] Title: 대표값2, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -9,33 +10,9 @@ class Main {
             arr[i] = Integer.parseInt(br.readLine());
             sum += arr[i];
         }
-        quickSort(arr, 0, 4);
+        Arrays.sort(arr);
         StringBuilder sb = new StringBuilder();
         sb.append(sum / 5).append("\n").append(arr[2]);
         System.out.println(sb);
-    }
-    
-    public static void quickSort(int[] arr, int l, int r) {
-        int part = partition(arr, l, r);
-        if (l < part - 1) quickSort(arr, l, part - 1);
-        if (part < r) quickSort(arr, part, r);
-    }
-    
-    public static int partition(int[] arr, int l, int r) {
-        int m = (l + r) / 2;
-        int povit = arr[m];
-        
-        while (l <= r) {
-            while (arr[l] < povit) l++;
-            while (povit < arr[r]) r--;
-            if (l <= r) {
-                int tmp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = tmp;
-                l++;
-                r--;
-            }
-        }
-        return l;
     }
 }
