3a48e2cb55860817c292247cec80bdfab9636d9b
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-08 19:52:23 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int[] alpha = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            int j = str.charAt(i) - 'a';
+            alpha[j]++;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            sb.append(alpha[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
