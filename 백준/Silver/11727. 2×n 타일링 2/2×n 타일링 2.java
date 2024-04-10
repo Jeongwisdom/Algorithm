701a323dae9341936c7be5e806a2ec5d396ef6ac
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-10 17:21:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int value = i % 2 == 0? 1: -1;
+            dp[i] = (dp[i - 1] * 2 + value) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
