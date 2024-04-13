1aa0b653e10eca9bf1b8e6e6ad10c98394ee5551
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-13 20:05:34 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -5,16 +5,18 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[][] dp = new int[n][10];
+        int mod = 1000000000;
         for (int i = 1; i <= 9; i++) {
             dp[0][i] = 1;
         }
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j <= 9; j++) {
-                if (j - 1 >= 0) dp[i][j] += dp[i - 1][j - 1];
-                if (j + 1 <= 9) dp[i][j] += dp[i - 1][j + 1];
-                dp[i][j] %= 1000000000;
+            dp[i][0] = dp[i - 1][1] % mod;
+            dp[i][9] = dp[i - 1][8] % mod;
+            for (int j = 1; j <= 8; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
             }
         }
+        
         long answer = 0;
         for (int i = 0; i <= 9; i++) {
             answer += dp[n - 1][i];
