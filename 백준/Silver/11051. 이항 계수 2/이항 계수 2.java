69df93bbbe389e575e4e600ab1911bd4cb3096e4
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-15 06:19:01 +0900

    [Silver II] Title: 이항 계수 2, Time: 136 ms, Memory: 18260 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+            dp[i][i] = 1;
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+            }
+        }
+        System.out.println(dp[n][k]);
+    }
+}
