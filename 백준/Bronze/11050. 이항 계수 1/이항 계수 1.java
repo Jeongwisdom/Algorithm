ff2c234b45b0a5a34173ca1ece97f0b87a7c7635
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-17 06:15:26 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -7,12 +7,12 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] dp = new int[n + 1][n + 1];
+        int[][] dp = new int[n + 1][k + 1];
         for (int i = 0; i <= n; i++) {
-            dp[i][0] = 1;
-            dp[i][i] = 1;
-            for (int j = 1; j < i; j++) {
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            for (int j = 0; j <= i; j++) {
+                if (j > k) break;
+                if (j == 0 || j == i) dp[i][j] = 1;
+                else dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
             }
         }
         System.out.println(dp[n][k]);
