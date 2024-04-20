15e839307f4fb49de7b7c1a16938d941cfd4c31d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-20 21:32:03 +0900

    [Bronze V] Title: 개수 세기, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -6,14 +6,11 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
         int v = Integer.parseInt(br.readLine());
         int answer = 0;
         for (int i = 0; i < n; i++) {
-            if (arr[i] == v) answer++;
+            int num = Integer.parseInt(st.nextToken());
+            if (num == v) answer++;
         }
         System.out.println(answer);
     }
