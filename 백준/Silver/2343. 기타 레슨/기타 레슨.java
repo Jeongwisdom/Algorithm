92ac1b2e731e4e182483995a1f10ec2ce95bd36e
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-21 14:20:36 +0900

    [Silver I] Title: 기타 레슨, Time: 164 ms, Memory: 14788 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 class Main {
     static int read() throws Exception {
-        int c, n = System.in.read() - 48;
-        while ((c = System.in.read()) > 32) n = (n * 10) + (c - 48);
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
     
