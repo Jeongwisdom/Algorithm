f1d4a167e06198b764c73817d4a266ed8834e6a6
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-24 06:44:45 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 304 ms, Memory: 19308 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.java
@@ -0,0 +1,42 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        long atk = read();
+        int[][] room = new int[n][3];
+        long min = 1;
+        long max = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                room[i][j] = read();
+            }
+            if (room[i][0] == 1) max += (room[i][1] * (room[i][2] / atk));
+        }
+
+        while (min <= max) {
+            long mid = (min + max) >> 1;
+            long hp = mid;
+            long cAtk = atk;
+            for (int i = 0; i < n; i++) {
+                if (room[i][0] == 1) {
+                    long num = room[i][2] / cAtk;
+                    if (room[i][2] % cAtk == 0) num--;
+                    hp -= (room[i][1] * num);
+                } else {
+                    cAtk += room[i][1];
+                    hp += room[i][2];
+                    if (hp > mid) hp = mid;
+                }
+                if (hp <= 0) break;
+            }
+            if (hp <= 0) min = mid + 1;
+            else max = mid - 1;
+        }
+        System.out.println(min);
+    }
+}
