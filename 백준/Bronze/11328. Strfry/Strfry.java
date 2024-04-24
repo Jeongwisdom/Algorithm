1fbfa2474ca73ae6167480e587ebda5d62608b16
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-25 07:08:46 +0900

    [Bronze II] Title: Strfry, Time: 312 ms, Memory: 21492 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -6,16 +6,15 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+            String[] split = br.readLine().split(" ");
             int[] arr1 = new int[26];
-            String str1 = st.nextToken();
+            String str1 = split[0];
             for (int j = 0; j < str1.length(); j++) {
                 arr1[str1.charAt(j) - 'a']++;
             }
             int[] arr2 = new int[26];
-            String str2 = st.nextToken();
+            String str2 = split[1];
             for (int j = 0; j < str2.length(); j++) {
                 arr2[str2.charAt(j) - 'a']++;
             }
