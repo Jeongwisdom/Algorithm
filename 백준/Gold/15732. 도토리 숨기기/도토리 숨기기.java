d642daad7982f7a906f994eb47b8f9f5ceb312d8
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-04-25 09:40:56 +0900

    [Gold II] Title: 도토리 숨기기, Time: 156 ms, Memory: 14716 KB -BaekjoonHub

---
+++ 백준/Gold/15732. 도토리 숨기기/도토리 숨기기.java
@@ -0,0 +1,35 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        int k = read();
+        int d = read();
+        int[][] arr = new int[k][3];
+        int min = 0;
+        int max = n;
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = read();
+            }
+        }
+
+        while (min <= max) {
+            int mid = (min + max) >> 1;
+            long num = 0;
+            for (int i = 0; i < k; i++) {
+                if (arr[i][0] <= mid) {
+                    int m = Math.min(arr[i][1], mid);
+                    num += (m - arr[i][0]) / arr[i][2] + 1;
+                }
+            }
+            if (d <= num) max = mid - 1;
+            else min = mid + 1;
+        }
+        System.out.println(min);
+    }
+}
