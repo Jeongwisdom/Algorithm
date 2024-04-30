f17852e8c1d66dd8e2e1bfcc2d75c481e94a7689
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-01 07:19:43 +0900

    [Silver II] Title: 키로거, Time: 760 ms, Memory: 89964 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -6,15 +6,12 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        Deque<Character> l, r;
-        String str;
-        char c;
         for (int i = 0; i < n; i++) {
-            str = br.readLine();
-            l = new ArrayDeque<>();
-            r = new ArrayDeque<>();
+            String str = br.readLine();
+            Deque<Character> l = new ArrayDeque<>();
+            Deque<Character> r = new ArrayDeque<>();
             for (int j = 0; j < str.length(); j++) {
-                c = str.charAt(j);
+                char c = str.charAt(j);
                 if (c == '<') {
                     if (!l.isEmpty()) r.offerFirst(l.pollLast());
                 } else if (c == '>') {
