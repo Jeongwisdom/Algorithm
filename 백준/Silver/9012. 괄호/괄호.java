8c90205bb7bf91f2887a293a9f6249d70adbadd5
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-06 17:55:25 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        Stack<Character> s;
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            s = new Stack<>();
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j) == '(') {
+                    s.push('(');
+                } else {
+                    if (s.isEmpty()) {
+                        s.push('(');
+                        break;
+                    }
+                    s.pop();
+                }
+            }
+            if (s.isEmpty()) sb.append("YES\n");
+            else sb.append("NO\n");
+        }
+        System.out.println(sb);
+    }
+}
