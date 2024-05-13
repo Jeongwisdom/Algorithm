24a0d546e30c45d859476a768b5a549f27712333
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-13 20:24:49 +0900

    [Platinum V] Title: 히스토그램, Time: 184 ms, Memory: 19692 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.java
+++ 백준/Platinum/1725. 히스토그램/히스토그램.java
@@ -11,16 +11,18 @@ class Main {
         Stack<int[]> s = new Stack<>();
         s.push(new int[] {0, 0});
         int n = read();
+        int[][] arr = new int[n + 2][2];
+        int idx = 0;
         int answer = 0;
         for (int i = 1; i <= n + 1; i++) {
             int num;
             if (i == n + 1) num = 0;
             else num = read();
-            while (s.size() > 1 && s.peek()[1] >= num) {
-                int[] a = s.pop();
-                answer = Math.max(answer, (i - (s.peek()[0] + 1)) * a[1]);
+            while (idx > 0 && arr[idx][1] >= num) {
+                answer = Math.max(answer, (i - (arr[idx - 1][0] + 1)) * arr[idx][1]);
+                idx--;
             }
-            s.push(new int[] {i, num});
+            arr[++idx] = new int[] {i, num};
         }
         System.out.println(answer);
     }
