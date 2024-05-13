0badfb13d391b2d4b5bd1a5ccc8a1e62dd69ee70
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-13 20:19:17 +0900

    [Platinum V] Title: 히스토그램, Time: 244 ms, Memory: 18652 KB -BaekjoonHub

--- 백준/Platinum/1725. 히스토그램/히스토그램.java
+++ 백준/Platinum/1725. 히스토그램/히스토그램.java
@@ -1,24 +1,26 @@
-import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 2];
-        for (int i = 1; i < n + 1; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        
-        int[] s = new int[n + 2];
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Stack<int[]> s = new Stack<>();
+        s.push(new int[] {0, 0});
+        int n = read();
         int answer = 0;
-        for (int i = 1, idx = 0; i < n + 2; i++) {
-            while (idx > 0 && arr[i] <= arr[s[idx]]) {
-                int height = arr[s[idx]];
-                idx--;
-                answer = Math.max(answer, height * (i - (s[idx] + 1)));
+        for (int i = 1; i <= n + 1; i++) {
+            int num;
+            if (i == n + 1) num = 0;
+            else num = read();
+            while (s.size() > 1 && s.peek()[1] >= num) {
+                int[] a = s.pop();
+                answer = Math.max(answer, (i - (s.peek()[0] + 1)) * a[1]);
             }
-            s[++idx] = i;
+            s.push(new int[] {i, num});
         }
         System.out.println(answer);
     }
