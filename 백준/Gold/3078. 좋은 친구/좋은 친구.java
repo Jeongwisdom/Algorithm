3da7b25839d99f7c5b560c8cdf32a248344cbbb7
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-15 16:20:11 +0900

    [Gold IV] Title: 좋은 친구, Time: 368 ms, Memory: 42448 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.java
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.java
@@ -7,13 +7,13 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        Map<Integer, Queue<Integer>> map = new HashMap<>();
-        for (int i = 2; i <= 20; i++) map.put(i, new ArrayDeque<>());
+        Queue<Integer>[] qs = new Queue[21];
+        for (int i = 2; i < 21; i++) qs[i] = new ArrayDeque<>();
         Queue<Integer> q;
         long answer = 0;
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            q = map.get(str.length());
+            q = qs[str.length()];
             q.offer(i);
             while (q.peek() < i - k) q.poll();
             answer += q.size() - 1;
