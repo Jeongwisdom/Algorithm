c87734e8ece77b73520ea1891f1fa0c520ee5003
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-16 14:55:29 +0900

    [Silver IV] Title: 덱, Time: 192 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.io.*;
 
 class Main {
@@ -8,39 +9,31 @@ class Main {
         int[] arr = new int[2 * n + 1];
         int h = n;
         int t = n;
-        String[] split;
+        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            split = br.readLine().split(" ");
-            switch (split[0]) {
-                case "push_front":
-                    arr[h--] = Integer.parseInt(split[1]);
-                    break;
-                case "push_back":
-                    arr[++t] = Integer.parseInt(split[1]);
-                    break;
-                case "pop_front":
-                    if (h == t) sb.append("-1\n");
-                    else sb.append(arr[++h]).append("\n");
-                    break;
-                case "pop_back":
-                    if (h == t) sb.append("-1\n");
-                    else sb.append(arr[t--]).append("\n");
-                    break;
-                case "size":
-                    sb.append(t - h).append("\n");
-                    break;
-                case "empty":
-                    if (t - h == 0) sb.append("1\n");
-                    else sb.append("0\n");
-                    break;
-                case "front":
-                    if (h == t) sb.append("-1\n");
-                    else sb.append(arr[h + 1]).append("\n");
-                    break;
-                default:
-                    if (h == t) sb.append("-1\n");
-                    else sb.append(arr[t]).append("\n");
-                    break;
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
+            if (str.equals("push_front")) {
+                arr[h--] = Integer.parseInt(st.nextToken());
+            } else if (str.equals("push_back")) {
+                arr[++t] = Integer.parseInt(st.nextToken());
+            } else if (str.equals("pop_front")) {
+                if (h == t) sb.append("-1\n");
+                else sb.append(arr[++h]).append("\n");
+            } else if (str.equals("pop_back")) {
+                if (h == t) sb.append("-1\n");
+                else sb.append(arr[t--]).append("\n");
+            } else if (str.equals("size")) {
+                sb.append(t - h).append("\n");
+            } else if (str.equals("empty")) {
+                if (t - h == 0) sb.append("1\n");
+                else sb.append("0\n");
+            } else if (str.equals("front")) {
+                if (h == t) sb.append("-1\n");
+                else sb.append(arr[h + 1]).append("\n");
+            } else {
+                if (h == t) sb.append("-1\n");
+                else sb.append(arr[t]).append("\n");
             }
         }
         System.out.println(sb);
