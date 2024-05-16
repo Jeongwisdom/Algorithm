a9b1c4ff454864cbcea0e06ee25f25d986ebe00c
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-16 14:53:04 +0900

    [Silver IV] Title: 덱, Time: 196 ms, Memory: 18924 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,4 +1,3 @@
-import java.util.*;
 import java.io.*;
 
 class Main {
@@ -12,27 +11,36 @@ class Main {
         String[] split;
         for (int i = 0; i < n; i++) {
             split = br.readLine().split(" ");
-            if (split[0].equals("push_front")) {
-                arr[h--] = Integer.parseInt(split[1]);
-            } else if (split[0].equals("push_back")) {
-                arr[++t] = Integer.parseInt(split[1]);
-            } else if (split[0].equals("pop_front")) {
-                if (h == t) sb.append("-1\n");
-                else sb.append(arr[++h]).append("\n");
-            } else if (split[0].equals("pop_back")) {
-                if (h == t) sb.append("-1\n");
-                else sb.append(arr[t--]).append("\n");
-            } else if (split[0].equals("size")) {
-                sb.append(t - h).append("\n");
-            } else if (split[0].equals("empty")) {
-                if (t - h == 0) sb.append("1\n");
-                else sb.append("0\n");
-            } else if (split[0].equals("front")) {
-                if (h == t) sb.append("-1\n");
-                else sb.append(arr[h + 1]).append("\n");
-            } else {
-                if (h == t) sb.append("-1\n");
-                else sb.append(arr[t]).append("\n");
+            switch (split[0]) {
+                case "push_front":
+                    arr[h--] = Integer.parseInt(split[1]);
+                    break;
+                case "push_back":
+                    arr[++t] = Integer.parseInt(split[1]);
+                    break;
+                case "pop_front":
+                    if (h == t) sb.append("-1\n");
+                    else sb.append(arr[++h]).append("\n");
+                    break;
+                case "pop_back":
+                    if (h == t) sb.append("-1\n");
+                    else sb.append(arr[t--]).append("\n");
+                    break;
+                case "size":
+                    sb.append(t - h).append("\n");
+                    break;
+                case "empty":
+                    if (t - h == 0) sb.append("1\n");
+                    else sb.append("0\n");
+                    break;
+                case "front":
+                    if (h == t) sb.append("-1\n");
+                    else sb.append(arr[h + 1]).append("\n");
+                    break;
+                default:
+                    if (h == t) sb.append("-1\n");
+                    else sb.append(arr[t]).append("\n");
+                    break;
             }
         }
         System.out.println(sb);
