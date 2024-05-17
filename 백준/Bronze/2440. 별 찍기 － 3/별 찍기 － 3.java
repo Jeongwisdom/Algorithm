b29639e547fe7dc0d05dee1c7db67ebd5b8b11f8
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-17 18:49:38 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -0,0 +1,19 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        int n = read();
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
