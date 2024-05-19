5b97731bb8f924df3c3442ad20a7103f1ef60e2e
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-19 17:28:46 +0900

    [Silver I] Title: 그림, Time: 304 ms, Memory: 34784 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Main {
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
@@ -16,32 +18,34 @@ class Main {
         }
         
         boolean[][] ch = new boolean[n][m];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         int count = 0;
         int area = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (arr[i][j] == 1 && !ch[i][j]) {
                     count++;
-                    area = Math.max(area, checkArea(arr, ch, i, j));
+                    int a = 0;
+                    Queue<int[]> q = new ArrayDeque<>();
+                    q.offer(new int[] {i, j});
+                    ch[i][j] = true;
+                    while (!q.isEmpty()) {
+                        int[] p = q.poll();
+                        a++;
+                        for (int k = 0; k < 4; k++) {
+                            int nx = p[0] + dx[k];
+                            int ny = p[1] + dy[k];
+                            if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 1 && !ch[nx][ny]) {
+                                q.offer(new int[] {nx, ny});
+                                ch[nx][ny] = true;
+                            }
+                        }
+                    }
+                    if (a > area) area = a;
                 }
             }
         }
         System.out.println(count + "\n" + area);
-    }
-    
-    static int checkArea(int[][] arr, boolean[][] ch, int x, int y) {
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        int area = 1;
-        ch[x][y] = true;
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < arr.length && ny >= 0 && ny < arr[0].length && arr[nx][ny] == 1 && !ch[nx][ny]) {
-                ch[nx][ny] = true;
-                area += checkArea(arr, ch, nx, ny);
-            }
-        }
-        return area;
     }
 }
