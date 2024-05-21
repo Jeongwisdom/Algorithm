2a3c8d3e91969c1c5a5e6e414e92105e6241c5ef
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-21 18:34:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 660 ms, Memory: 141580 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -7,36 +7,35 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        boolean[][] arr = new boolean[n + 1][n + 1];
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> l;
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = true;
-            arr[b][a] = true;
+            list.get(a).add(b);
+            list.get(b).add(a);
         }
-        for (int i = 1; i <= n; i++) {
-            arr[i][i] = true;
-        }
-        boolean[][] ch = new boolean[n + 1][n + 1];
+        boolean[] ch = new boolean[n + 1];
         Queue<Integer> q = new LinkedList<>();
         int answer = 0;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (arr[i][j] && !ch[i][j]) {
-                    answer++;
-                    q.offer(i);
-                    while (!q.isEmpty()) {
-                        int a = q.poll();
-                        for (int b = j; b <= n; b++) {
-                            if (arr[a][b] && !ch[a][b]) {
-                                ch[a][b] = true;
-                                ch[b][a] = true;
-                                q.offer(b);
-                            }
-                        }
+            if (!ch[i]) {
+                answer++;
+                ch[i] = true;
+                q.offer(i);
+            }
+            while (!q.isEmpty()) {
+                int a = q.poll();
+                l = list.get(a);
+                for (int b: l) {
+                    if (!ch[b]) {
+                        ch[b] = true;
+                        q.offer(b);
                     }
-                    break;
                 }
             }
         }
