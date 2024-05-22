face66dea16b3ff1b69effaef1a2eb8757ede8c3
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-22 21:23:36 +0900

    [level 2] Title: 광물 캐기, Time: 0.89 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,31 +1,37 @@
+import java.util.*;
+
 class Solution {
-    int answer = 10000;
-    
     public int solution(int[] picks, String[] minerals) {
-        DFS(picks, minerals, 0, 0);
-        return answer;
-    }
-    
-    void DFS(int[] picks, String[] minerals, int l, int a) {
-        if (l >= minerals.length || (picks[0] == 0 && picks[1] == 0 && picks[2] == 0)) {
-            if (answer > a) answer = a;
-        } else {
-            for (int i = 2; i >= 0; i--) {
-                if (picks[i] > 0) {
-                    picks[i]--;
-                    int sum = 0;
-                    for (int j = l; j < l + 5; j++) {
-                        if (j == minerals.length) break;
-                        int mineral = 0;
-                        if (minerals[j].equals("iron")) mineral = 1;
-                        else if (minerals[j].equals("stone")) mineral = 2;
-                        int c = (int) Math.pow(5, i - mineral <= 0? 0: i - mineral);
-                        sum += c;
-                    }
-                    DFS(picks, minerals, l + 5, a + sum);
-                    picks[i]++;
-                }
+        int count = picks[0] + picks[1] + picks[2];
+        int[][] arr = new int[minerals.length / 5 + 1][3];
+        for (int i = 0, id = 0; i < minerals.length; i += 5, id++, count--) {
+            if (count == 0) break;
+            int[] mineral = new int[3];
+            for (int j = i; j < i + 5; j++) {
+                if (j == minerals.length) break;
+                if (minerals[j].equals("stone")) mineral[2]++;
+                else if (minerals[j].equals("iron")) mineral[1]++;
+                else mineral[0]++;
             }
+            arr[id] = mineral;
         }
+        
+        int answer = 0;
+        Arrays.sort(arr, (a1, a2) -> {
+            if (a2[0] == a1[0]) {
+                if (a2[1] == a1[1]) return a2[2] - a1[2];
+                return a2[1] - a1[1];
+            }
+            return a2[0] - a1[0];
+        });
+        for (int i = 0; i < arr.length; i++) {
+            if (picks[0] == 0 && picks[1] == 0 && picks[2] == 0) break;
+            int id = 2;
+            if (picks[0] > 0) id = 0;
+            else if (picks[1] > 0) id = 1;
+            picks[id]--;
+            answer += arr[i][0] * Math.pow(5, id) + arr[i][1] * Math.pow(5, id - 1 <= 0? 0: id - 1) + arr[i][2] * Math.pow(5, id - 2 <= 0? 0: id - 2);
+        }
+        return answer;
     }
 }
