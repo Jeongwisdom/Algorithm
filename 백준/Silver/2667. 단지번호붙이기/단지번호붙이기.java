a502c6c950cd06486a73d0581fe3fc49afa69f0c
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-23 15:24:25 +0900

    [Silver I] Title: 단지번호붙이기, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+        Queue<Integer> pq = new PriorityQueue<>();
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] ch = new boolean[n][n];
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[i].charAt(j) == '1' && !ch[i][j]) {
+                    ch[i][j] = true;
+                    q.offer(new int[] {i, j});
+                    int area = 0;
+                    while (!q.isEmpty()) {
+                        area++;
+                        int[] point = q.poll();
+                        for (int k = 0; k < 4; k++) {
+                            int nx = point[0] + dx[k];
+                            int ny = point[1] + dy[k];
+                            if (nx >= 0 && nx < n && ny >= 0 && ny < n && arr[nx].charAt(ny) == '1' && !ch[nx][ny]) {
+                                ch[nx][ny] = true;
+                                q.offer(new int[] {nx, ny});
+                            }
+                        }
+                    }
+                    pq.offer(area);
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(pq.size()).append("\n");
+        while (!pq.isEmpty()) {
+            sb.append(pq.poll()).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
