175034ed53e8f69487291b4ac677bba3ad7d928a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-25 07:39:44 +0900

    [Gold V] Title: 적록색약, Time: 180 ms, Memory: 17352 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(check(arr, n)).append(" ");
+        for (int i = 0; i < n; i++) arr[i] = arr[i].replaceAll("G", "R");
+        sb.append(check(arr, n));
+        System.out.println(sb);
+    }
+    
+    static int check(String[] arr, int n) {
+        int answer = 0;
+        boolean[][] ch = new boolean[n][n];
+        Queue<int[]> q = new ArrayDeque<>();
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                char c = arr[i].charAt(j);
+                if (!ch[i][j]) {
+                    ch[i][j] = true;
+                    answer++;
+                    q.offer(new int[] {i, j});
+                    while (!q.isEmpty()) {
+                        int[] point = q.poll();
+                        for (int k = 0; k < 4; k++) {
+                            int nx = point[0] + dx[k];
+                            int ny = point[1] + dy[k];
+                            if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx].charAt(ny) == c && !ch[nx][ny]) {
+                                ch[nx][ny] = true;
+                                q.offer(new int[] {nx, ny});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
