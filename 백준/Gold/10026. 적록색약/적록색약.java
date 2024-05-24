397e0e7d4af1122303f46b29a16811745874cb93
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-25 07:57:59 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 14608 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -5,16 +5,21 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        StringBuilder[] arr = new StringBuilder[n];
+        char[][] arr1 = new char[n][n];
+        char[][] arr2 = new char[n][n];
         for (int i = 0; i < n; i++) {
-            arr[i] = new StringBuilder(br.readLine());
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr1[i][j] = str.charAt(j);
+                arr2[i][j] = arr1[i][j] == 'G'? 'R': arr1[i][j];
+            }
         }
         StringBuilder sb = new StringBuilder();
-        sb.append(check(arr, n)).append(" ").append(check(arr, n));
+        sb.append(check(arr1, n)).append(" ").append(check(arr2, n));
         System.out.println(sb);
     }
     
-    static int check(StringBuilder[] arr, int n) {
+    static int check(char[][] arr, int n) {
         int answer = 0;
         boolean[][] ch = new boolean[n][n];
         Queue<int[]> q = new ArrayDeque<>();
@@ -22,7 +27,7 @@ class Main {
         int[] dy = {0, 0, -1, 1};
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                char c = arr[i].charAt(j);
+                char c = arr[i][j];
                 if (!ch[i][j]) {
                     ch[i][j] = true;
                     answer++;
@@ -32,14 +37,13 @@ class Main {
                         for (int k = 0; k < 4; k++) {
                             int nx = point[0] + dx[k];
                             int ny = point[1] + dy[k];
-                            if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx].charAt(ny) == c && !ch[nx][ny]) {
+                            if (nx >= 0 && ny >= 0 && nx < n && ny < n && arr[nx][ny] == c && !ch[nx][ny]) {
                                 ch[nx][ny] = true;
                                 q.offer(new int[] {nx, ny});
                             }
                         }
                     }
                 }
-                if (c == 'G') arr[i].setCharAt(j, 'R');
             }
         }
         return answer;
