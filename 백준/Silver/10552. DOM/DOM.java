efb8f4467f9fd997bc6121deb64250f14c31db7a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-26 23:07:11 +0900

    [Silver II] Title: DOM, Time: 364 ms, Memory: 37932 KB -BaekjoonHub

--- 백준/Silver/10552. DOM/DOM.java
+++ 백준/Silver/10552. DOM/DOM.java
@@ -8,27 +8,22 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int p = Integer.parseInt(st.nextToken());
-        Map<Integer, List<Integer>> hated = new HashMap<>();
-        List<Integer> list;
-        int[] arr = new int[n];
+        int[] arr = new int[m + 1];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+            int favorite = Integer.parseInt(st.nextToken());
             int hate = Integer.parseInt(st.nextToken());
-            list = hated.getOrDefault(hate, new ArrayList<>());
-            list.add(i);
-            hated.put(hate, list);
+            if (arr[hate] == 0) arr[hate] = favorite;
         }
-        list = hated.getOrDefault(p, new ArrayList<>());
         int answer = 0;
         boolean[] ch = new boolean[m + 1];
-        while (list.size() > 0) {
-            if (ch[list.get(0)]) {
+        while (arr[p] > 0) {
+            if (ch[p]) {
                 answer = -1;
                 break;
             }
-            ch[list.get(0)] = true;
-            list = hated.getOrDefault(arr[list.get(0)], new ArrayList<>());
+            ch[p] = true;
+            p = arr[p];
             answer++;
         }
         System.out.println(answer);
