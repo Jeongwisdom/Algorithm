ab265099fcc217a610d0e1e2325c2a160257f3dd
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-27 18:09:27 +0900

    [Silver I] Title: 숨바꼭질, Time: 188 ms, Memory: 27740 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int max = Math.max(n, k);
+        boolean[] ch = new boolean[(max + 1) * 2];
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(k);
+        int answer = 0;
+        loop:
+        while (!q.isEmpty()) {
+            Queue<Integer> tmp = new ArrayDeque<>();
+            while (!q.isEmpty()) {
+                int num = q.poll();
+                if (num == n) break loop;
+                if (!ch[num]) {
+                    ch[num] = true;
+                    if (num < ch.length - 1 && !ch[num + 1]) tmp.offer(num + 1);
+                    if (num >= 1 && !ch[num - 1]) tmp.offer(num - 1);
+                    if (num % 2 == 0 && !ch[num / 2]) tmp.offer(num / 2);
+                }
+            }
+            q = tmp;
+            answer++;
+        }
+        System.out.println(answer);
+    }
+}
