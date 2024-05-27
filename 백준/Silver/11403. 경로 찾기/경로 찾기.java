a754f2972a3b0aae8bf49a37cd237dbb4b069ad9
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-27 21:54:56 +0900

    [Silver I] Title: 경로 찾기, Time: 216 ms, Memory: 16320 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -5,12 +5,12 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        char[][] arr = new char[n][n];
+        boolean[][] arr = new boolean[n][n];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                arr[i][j] = st.nextToken().charAt(0);
+                arr[i][j] = st.nextToken().charAt(0) == '1';
             }
         }
         int[] ch = new int[n];
@@ -21,21 +21,21 @@ class Main {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                sb.append(arr[i][j]).append(" ");
+                sb.append(arr[i][j]? 1: 0).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
     }
 
-    static void DFS(char[][] arr, int[] ch, int id, int n) {
+    static void DFS(boolean[][] arr, int[] ch, int id, int n) {
         if (ch[id] < 2) {
             ch[id]++;
             for (int i = 0; i < n; i++) {
-                if (arr[id][i] == '1') {
+                if (arr[id][i]) {
                     DFS(arr, ch, i, n);
                     for (int j = 0; j < n; j++) {
-                        if (arr[i][j] == '1') arr[id][j] = '1';
+                        if (arr[i][j]) arr[id][j] = true;
                     }
                 }
             }
