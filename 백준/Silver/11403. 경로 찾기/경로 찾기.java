f58d119f5dff1c99964964ae795a01554ee033e4
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-27 23:48:02 +0900

    [Silver I] Title: 경로 찾기, Time: 180 ms, Memory: 15436 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -5,40 +5,30 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        boolean[][] arr = new boolean[n][n];
+        char[][] arr = new char[n][n];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                arr[i][j] = st.nextToken().charAt(0) == '1';
+                arr[i][j] = st.nextToken().charAt(0);
             }
         }
-        int[] ch = new int[n];
+        
         for (int i = 0; i < n; i++) {
-            DFS(arr, ch, i, n);
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (arr[j][i] == '1' && arr[i][k] == '1') arr[j][k] = '1';
+                }
+            }
         }
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                sb.append(arr[i][j]? 1: 0).append(" ");
+                sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
         }
         System.out.println(sb);
-    }
-
-    static void DFS(boolean[][] arr, int[] ch, int id, int n) {
-        if (ch[id] < 2) {
-            ch[id]++;
-            for (int i = 0; i < n; i++) {
-                if (arr[id][i]) {
-                    DFS(arr, ch, i, n);
-                    for (int j = 0; j < n; j++) {
-                        if (arr[i][j]) arr[id][j] = true;
-                    }
-                }
-            }
-        }
     }
 }
