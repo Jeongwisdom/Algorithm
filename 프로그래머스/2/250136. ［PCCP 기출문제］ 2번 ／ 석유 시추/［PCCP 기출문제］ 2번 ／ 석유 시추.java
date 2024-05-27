92b4454f8485dbc2b66ce07aa35dd62ee9e0dfb2
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-27 19:50:51 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 34.22 ms, Memory: 68.1 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -4,42 +4,48 @@ class Solution {
     public int solution(int[][] land) {
         int n = land.length;
         int m = land[0].length;
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+        boolean[][] ch = new boolean[n][m];
         int id = 2;
         int[] areas = new int[250000];
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
         Queue<int[]> q = new ArrayDeque<>();
-        int answer = 0;
-        for (int j = 0; j < m; j++) {
-            Set<Integer> set = new HashSet<>();
-            for (int i = 0; i < n; i++) {
-                if (land[i][j] == 1) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (land[i][j] == 1 && !ch[i][j]) {
+                    ch[i][j] = true;
                     q.offer(new int[] {i, j});
-                    land[i][j] = id;
                     int area = 0;
                     while (!q.isEmpty()) {
                         int[] point = q.poll();
+                        land[point[0]][point[1]] = id;
                         area++;
+                        Set<Integer> set = map.getOrDefault(point[1], new HashSet<>());
+                        set.add(id);
+                        map.put(point[1], set);
                         for (int k = 0; k < 4; k++) {
                             int nx = point[0] + dx[k];
                             int ny = point[1] + dy[k];
-                            if (nx >= 0 && ny >= 0 && nx < n && ny < m && land[nx][ny] == 1) {
-                                land[nx][ny] = id;
+                            if (nx >= 0 && ny >= 0 && nx < n && ny < m && land[nx][ny] == 1 && !ch[nx][ny]) {
+                                ch[nx][ny] = true;
                                 q.offer(new int[] {nx, ny});
                             }
                         }
                     }
-                    set.add(id);
                     areas[id++] = area;
-                } else set.add(land[i][j]);
+                }
             }
-            int c = 0;
-            for (int num: set) {
-                c += areas[num];
-            }
-            if (answer < c) answer = c;
         }
         
+        int answer = 0;
+        for (int i = 0; i < m; i++) {
+            int area = 0;
+            for (int num: map.getOrDefault(i, new HashSet<>())) {
+                area += areas[num];
+            }
+            if (answer < area) answer = area;
+        }
         return answer;
     }
 }
