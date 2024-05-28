3c535ab1c039efd8f989ca09be644d912c0bda6d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-28 20:24:24 +0900

    [Silver I] Title: 안전 영역, Time: 240 ms, Memory: 18176 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -9,30 +9,38 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][n];
+        int l = 100;
+        int r = 1;
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
+                if (arr[i][j] < l) l = arr[i][j];
+                else if (arr[i][j] > r) r = arr[i][j];
             }
         }
         
         int answer = 1;
-        boolean[][] ch;
-        for (int k = 1; k <= 100; k++) {
-            int count = 0;
-            ch = new boolean[n][n];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (!ch[i][j] && arr[i][j] > k) {
-                        count++;
-                        DFS(arr, ch, i, j, n, k);
-                    }
-                }
-            }
-            if (answer < count) answer = count;
+        for (int i = l; i < r; i++) {
+            int count = count(arr, n, i);
+            if (answer <= count) answer = count;
         }
         System.out.println(answer);
+    }
+    
+    static int count(int[][] arr, int n, int k) {
+        int count = 0;
+        boolean[][] ch = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!ch[i][j] && arr[i][j] > k) {
+                    count++;
+                    DFS(arr, ch, i, j, n, k);
+                }
+            }
+        }
+        return count;
     }
     
     static void DFS(int[][] arr, boolean[][] ch, int x, int y, int n, int mid) {
