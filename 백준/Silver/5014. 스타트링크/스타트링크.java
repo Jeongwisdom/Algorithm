423ff669e5e7c9e7c958ff72af8c778b8ed815e0
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-05-30 17:31:25 +0900

    [Silver I] Title: 스타트링크, Time: 288 ms, Memory: 142024 KB -BaekjoonHub

---
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int f = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int g = Integer.parseInt(st.nextToken());
+        int u = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        boolean[] ch = new boolean[f + 1];
+        Queue<Integer> q = new ArrayDeque<>();
+        int answer = 0;
+        ch[s] = true;
+        q.offer(s);
+        loop:
+        while (!q.isEmpty()) {
+            Queue<Integer> tmp = new ArrayDeque<>();
+            while (!q.isEmpty()) {
+                int num = q.poll();
+                if (num == g) break loop;
+                if (num + u <= f && !ch[num + u]) {
+                    ch[num + u] = true;
+                    tmp.offer(num + u);
+                }
+                if (num - d > 0 && !ch[num - d]) {
+                    ch[num - d] = true;
+                    tmp.offer(num - d);
+                }
+            }
+            q = tmp;
+            answer++;
+        }
+        System.out.println(!ch[g]? "use the stairs": answer);
+    }
+}
