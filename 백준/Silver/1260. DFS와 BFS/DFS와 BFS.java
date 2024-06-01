3fcb5057d0dcdab0def27cd88be9071328a5b911
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-02 07:12:36 +0900

    [Silver II] Title: DFS와 BFS, Time: 240 ms, Memory: 20084 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -10,44 +10,50 @@ class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int v = Integer.parseInt(st.nextToken());
-        boolean[][] dot = new boolean[n + 1][n + 1];
+        List<List<Integer>> dot = new ArrayList<>();
+        for (int i = 0; i <= n; i++) dot.add(new ArrayList<>());
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            dot[a][b] = true;
-            dot[b][a] = true;
+            dot.get(a).add(b);
+            dot.get(b).add(a);
         }
+        for (int i = 1; i <= n; i++) Collections.sort(dot.get(i));
         boolean[] ch = new boolean[n + 1];
         ch[v] = true;
         DFS(dot, v, ch);
-        sb.append("\n").append(BFS(dot, v));
+        sb.append("\n").append(BFS(dot, v, n));
         System.out.println(sb);
     }
 
-    static void DFS(boolean[][] dot, int v, boolean[] ch) {
+    static void DFS(List<List<Integer>> dot, int v, boolean[] ch) {
         sb.append(v).append(" ");
-        for (int i = 0; i < dot.length; i++) {
-            if (!ch[i] && dot[v][i]) {
-                ch[i] = true;
-                DFS(dot, i, ch);
+        List<Integer> list = dot.get(v);
+        for (int i = 0; i < list.size(); i++) {
+            int id = list.get(i);
+            if (!ch[id]) {
+                ch[id] = true;
+                DFS(dot, id, ch);
             }
         }
     }
 
-    static String BFS(boolean[][] dot, int v) {
+    static String BFS(List<List<Integer>> dot, int v, int n) {
         Deque<Integer> q = new ArrayDeque<>();
-        boolean[] ch = new boolean[dot.length];
+        boolean[] ch = new boolean[n + 1];
         ch[v] = true;
         q.offer(v);
         StringBuilder sb = new StringBuilder();
         while (!q.isEmpty()) {
             int num = q.poll();
             sb.append(num).append(" ");
-            for (int i = 0; i < dot.length; i++) {
-                if (!ch[i] && dot[num][i]) {
-                    ch[i] = true;
-                    q.offer(i);
+            List<Integer> list = dot.get(num);
+            for (int i = 0; i < list.size(); i++) {
+                int id = list.get(i);
+                if (!ch[id]) {
+                    ch[id] = true;
+                    q.offer(id);
                 }
             }
         }
