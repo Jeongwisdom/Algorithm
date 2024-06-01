8d93237670758f25ae606342385a788b48844b88
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-02 00:50:58 +0900

    [Gold III] Title: 텀 프로젝트, Time: 616 ms, Memory: 44832 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,17 +1,18 @@
-import java.util.*;
-import java.io.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
         StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
+        int t = read();
         for (int c = 0; c < t; c++) {
-            int n = Integer.parseInt(br.readLine());
+            int n = read();
             int[] arr = new int[n + 1];
-            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 1; i <= n; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
+                arr[i] = read();
             }
             boolean[] ch = new boolean[n + 1];
             boolean[] visited = new boolean[n + 1];
