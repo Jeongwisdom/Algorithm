734c8635b21109fe78937ff9c8164ccac3280f4c
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-02 00:38:54 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1096 ms, Memory: 304872 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -14,20 +14,29 @@ class Main {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
             boolean[] ch = new boolean[n + 1];
-            Deque<Integer> q;
+            boolean[] visited = new boolean[n + 1];
             int answer = 0;
             for (int i = 1; i <= n; i++) {
-                q = new ArrayDeque<>();
-                q.offer(i);
-                while (!ch[q.peekLast()]) {
-                    ch[q.peekLast()] = true;
-                    q.offer(arr[q.peekLast()]);
-                }
-                while (!Objects.equals(q.peek(), q.peekLast())) q.poll();
-                answer += q.size() - 1;
+                if (!ch[i]) answer += DFS(arr, ch, visited, i);
             }
             sb.append(n - answer).append("\n");
         }
         System.out.println(sb);
+    }
+    
+    static int DFS(int[] arr, boolean[] ch, boolean[] visited, int id) {
+        int count = 0;
+        if (!ch[id]) {
+            ch[id] = true;
+            count += DFS(arr, ch, visited, arr[id]);
+            visited[id] = true;
+        } else {
+            while (!visited[id]) {
+                visited[id] = true;
+                count++;
+                id = arr[id];
+            }
+        }
+        return count;
     }
 }
