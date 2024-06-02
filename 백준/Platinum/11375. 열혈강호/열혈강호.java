1faa9a5c2e4411fb1c0fe5c86f09417e465981ad
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-02 15:44:32 +0900

    [Platinum IV] Title: 열혈강호, Time: 432 ms, Memory: 19376 KB -BaekjoonHub

--- 백준/Platinum/11375. 열혈강호/열혈강호.java
+++ 백준/Platinum/11375. 열혈강호/열혈강호.java
@@ -2,26 +2,28 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        int m = read();
         int[][] arr = new int[n + 1][];
         for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
+            int w = read();
             arr[i] = new int[w];
             for (int j = 0; j < w; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = read();
             }
         }
-        boolean[] ch;
+        
         int[] work = new int[m + 1];
         int answer = 0;
         for (int i = 1; i <= n; i++) {
-            ch = new boolean[m + 1];
-            if (DFS(arr, work, ch, i)) answer++;
+            if (DFS(arr, work, new boolean[m + 1], i)) answer++;
         }
         System.out.println(answer);
     }
