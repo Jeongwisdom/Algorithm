452a2e617acd2b5f2254e6b58a3d730acd6fcf31
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-06 06:57:10 +0900

    [Gold IV] Title: 불, Time: 608 ms, Memory: 59496 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -6,29 +6,25 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         StringTokenizer st;
-        for (int a = 0; a < t; a++) {
+        for (int c = 0; c < t; c++) {
             st = new StringTokenizer(br.readLine());
             int w = Integer.parseInt(st.nextToken());
             int h = Integer.parseInt(st.nextToken());
-            String[] arr = new String[h];
+            char[][] arr = new char[h][w];
             Queue<int[]> q = new ArrayDeque<>();
-            boolean[][] ch = new boolean[h][w];
             int[] s = new int[3];
             for (int i = 0; i < h; i++) {
-                arr[i] = br.readLine();
+                String str = br.readLine();
                 for (int j = 0; j < w; j++) {
-                    char c = arr[i].charAt(j);
-                    if (c == '*') {
-                        q.offer(new int[] {i, j, '*'});
-                        ch[i][j] = true;
-                    } else if (c == '@') s = new int[] {i, j, '@'};
+                    arr[i][j] = str.charAt(j);
+                    if (arr[i][j] == '*') q.offer(new int[] {i, j});
+                    else if (arr[i][j] == '@') s = new int[] {i, j};
                 }
             }
             q.offer(s);
-            ch[s[0]][s[1]] = true;
-            int[] dx = {-1, 1, 0, 0};
-            int[] dy = {0, 0, -1, 1};
             int answer = 0;
             loop:
             while (!q.isEmpty()) {
@@ -40,14 +36,14 @@ class Main {
                         int nx = point[0] + dx[j];
                         int ny = point[1] + dy[j];
                         if (nx < 0 || ny < 0 || nx >= h || ny >= w) {
-                            if ((char) point[2] == '@') {
+                            if (arr[point[0]][point[1]] == '@') {
                                 q.offer(new int[] {0});
                                 break loop;
                             }
                         } else {
-                            if (!ch[nx][ny] && arr[nx].charAt(ny) == '.') {
-                                ch[nx][ny] = true;
-                                q.offer(new int[] {nx, ny, point[2]});
+                            if (arr[nx][ny] == '.') {
+                                arr[nx][ny] = arr[point[0]][point[1]];
+                                q.offer(new int[] {nx, ny});
                             }
                         }
                     }
