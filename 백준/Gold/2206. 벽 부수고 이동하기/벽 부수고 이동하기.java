11b50eda2b5d75a75d00e83e3675b311afb86bff
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-12 07:33:56 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 516 ms, Memory: 79252 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,53 +2,50 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int n, m;
+    static String[] arr;
+    static boolean[][][] ch;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        String[] arr = new String[n];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new String[n];
         for (int i = 0; i < n; i++) {
             arr[i] = br.readLine();
         }
-        boolean[][][] ch = new boolean[2][n][m];
+        ch = new boolean[2][n][m];
+        System.out.println(BFS());
+    }
+    
+    static int BFS() {
         Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[] {0, 0, 0});
         ch[0][0][0] = true;
-        int answer = 0;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        loop:
+        int nx, ny, len, count = 0;
+        int[] p;
         while (!q.isEmpty()) {
-            int len = q.size();
-            answer++;
+            count++;
+            len = q.size();
             for (int i = 0; i < len; i++) {
-                int[] point = q.poll();
-                ch[point[2]][point[0]][point[1]] = true;
-                if (point[0] == n - 1 && point[1] == m - 1) {
-                    q.offer(new int[] {1});
-                    break loop;
-                }
+                p = q.poll();
+                if (p[0] == n - 1 && p[1] == m - 1) return count;
                 for (int j = 0; j < 4; j++) {
-                    int nx = point[0] + dx[j];
-                    int ny = point[1] + dy[j];
+                    nx = p[0] + dx[j];
+                    ny = p[1] + dy[j];
                     if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                        if (arr[nx].charAt(ny) == '0') {
-                            if (!ch[point[2]][nx][ny]) {
-                                ch[point[2]][nx][ny] = true;
-                                q.offer(new int[] {nx, ny, point[2]});
-                            }
-                        } else {
-                            if (point[2] == 0 && !ch[1][nx][ny]) {
-                                ch[1][nx][ny] = true;
-                                q.offer(new int[] {nx, ny, 1});
-                            }
+                        if (!ch[p[2]][nx][ny]) {
+                            ch[p[2]][nx][ny] = true;
+                            if (arr[nx].charAt(ny) == '0') q.offer(new int[] {nx, ny, p[2]});
+                            else if (p[2] == 0) q.offer(new int[] {nx, ny, 1});
                         }
                     }
                 }
             }
         }
-        if (q.isEmpty()) answer = -1;
-        System.out.println(answer);
+        return -1;
     }
 }
