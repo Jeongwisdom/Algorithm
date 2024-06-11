45b5471ce3ddf62eea3c452264d1305fd098357a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-11 12:04:19 +0900

    [Gold IV] Title: 빙산, Time: 388 ms, Memory: 47660 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.java
+++ 백준/Gold/2573. 빙산/빙산.java
@@ -4,13 +4,16 @@ import java.util.*;
 class Main {
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
+    static int n, m;
+    static int[][] arr;
+    static boolean[][] ch;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[n][m];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
@@ -23,31 +26,30 @@ class Main {
         while (ice == 1) {
             answer++;
             ice = 0;
-            boolean[][] ch = new boolean[n][m];
-            int[][] tmp = new int[n][m];
+            ch = new boolean[n][m];
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (!ch[i][j] && arr[i][j] > 0) {
                         ice++;
-                        DFS(arr, tmp, ch, i, j, n, m);
+                        DFS(i, j);
                     }
                 }
             }
-            arr = tmp;
         }
         if (ice == 0) answer = 0;
         System.out.println(answer);
     }
     
-    static void DFS(int[][] arr, int[][] tmp, boolean[][] ch, int x, int y, int n, int m) {
+    static void DFS(int x, int y) {
         ch[x][y] = true;
-        tmp[x][y] = arr[x][y];
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                if (arr[nx][ny] <= 0) tmp[x][y]--;
-                else if (!ch[nx][ny]) DFS(arr, tmp, ch, nx, ny, n, m);
+                if (!ch[nx][ny]) {
+                    if (arr[nx][ny] <= 0) arr[x][y]--;
+                    else DFS(nx, ny);
+                }
             }
         }
     }
