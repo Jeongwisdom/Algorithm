683b675bdfe40a10b2ebb3e41d15b63a175dfa20
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-14 10:01:11 +0900

    [Gold IV] Title: DSLR, Time: 2004 ms, Memory: 312456 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int n, a, b;
     static int[] id;
     static char[] c;
     static Queue<Integer> q;
@@ -10,42 +9,43 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringTokenizer st;
+        int a, b;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             a = Integer.parseInt(st.nextToken());
             b = Integer.parseInt(st.nextToken());
-            sb.append(BFS()).append("\n");
+            
+            q = new ArrayDeque<>();
+            id = new int[10000];
+            c = new char[10000];
+            q.offer(a);
+            while (!q.isEmpty()) {
+                int num = q.poll();
+                int twice = num * 2 % 10000;
+                add(twice, num, 'D');
+                if (twice == b) break;
+                int minus = num == 0? 9999: num - 1;
+                add(minus, num, 'S');
+                if (minus == b) break;
+                int l = num % 1000 * 10 + num / 1000;
+                add(l, num, 'L');
+                if (l == b) break;
+                int r = num % 10 * 1000 + num / 10;
+                add(r, num, 'R');
+                if (r == b) break;
+            }
+            
+            StringBuilder tmp = new StringBuilder();
+            int idx = b;
+            while (idx != a) {
+                tmp.append(c[idx]);
+                idx = id[idx] - 1;
+            }
+            sb.append(tmp.reverse().toString()).append("\n");
         }
         System.out.println(sb);
-    }
-    
-    static String BFS() {
-        q = new ArrayDeque<>();
-        id = new int[10000];
-        c = new char[10000];
-        q.offer(a);
-        while (!q.isEmpty()) {
-            int num = q.poll();
-            if (num == b) break;
-            int twice = num * 2 % 10000;
-            add(twice, num, 'D');
-            int minus = num == 0? 9999: num - 1;
-            add(minus, num, 'S');
-            int l = num % 1000 * 10 + num / 1000;
-            add(l, num, 'L');
-            int r = num % 10 * 1000 + num / 10;
-            add(r, num, 'R');
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        int idx = b;
-        while (idx != a) {
-            sb.append(c[idx]);
-            idx = id[idx] - 1;
-        }
-        return sb.reverse().toString();
     }
     
     static void add(int num, int ex, char command) {
