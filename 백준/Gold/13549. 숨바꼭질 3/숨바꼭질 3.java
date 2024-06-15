5b37c868485e72bbb3c5917185652c2d3f959354
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-15 09:09:32 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 16008 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -2,37 +2,41 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+    static int n, k;
+    static int[] dp;
+    static Deque<Integer> q;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] split = br.readLine().split(" ");
-        int n = Integer.parseInt(split[0]);
-        int k = Integer.parseInt(split[1]);
-        int[] dp = new int[100001];
-        Deque<Integer> q = new ArrayDeque<>();
+        n = Integer.parseInt(split[0]);
+        k = Integer.parseInt(split[1]);
+        dp = new int[100001];
+        q = new ArrayDeque<>();
         q.offer(k);
         dp[k] = 1;
         while (!q.isEmpty()) {
             int num = q.poll();
             int teleport = num / 2;
-            if (num % 2 == 0 && teleport >= 0 && teleport < 100001 && dp[teleport] == 0) {
-                dp[teleport] = dp[num];
-                q.offer(teleport);
+            if (num % 2 == 0) {
+                add(teleport, dp[num]);
                 if (teleport == n) break;
             }
             int plus = num + 1;
-            if (plus >= 0 && plus < 100001 && dp[plus] == 0) {
-                dp[plus] = dp[num] + 1;
-                q.offer(plus);
-                if (plus == n) break;
-            }
+            add(plus, dp[num] + 1);
+            if (plus == n) break;
             int minus = num - 1;
-            if (minus >= 0 && minus < 100001 && dp[minus] == 0) {
-                dp[minus] = dp[num] + 1;
-                q.offer(minus);
-                if (minus == n) break;
-            }
+            add(minus, dp[num] + 1);
+            if (minus == n) break;
         }
         dp[n]--;
         System.out.println(dp[n]);
+    }
+    
+    static void add(int num, int step) {
+        if (num >= 0 && num < 100001 && dp[num] == 0) {
+            dp[num] = step;
+            q.offer(num);
+        }
     }
 }
