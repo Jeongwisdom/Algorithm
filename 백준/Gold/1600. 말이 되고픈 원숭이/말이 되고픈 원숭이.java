fb3a40ae8319aed87e2b7cc0d7c4426f648df162
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-17 15:47:02 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 708 ms, Memory: 67928 KB -BaekjoonHub

---
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int k, w, h;
+    static int[][][] arr;
+    static Deque<int[]> q;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        k = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        w = Integer.parseInt(st.nextToken());
+        h = Integer.parseInt(st.nextToken());
+        arr = new int[k + 1][h][w];
+        for (int i = 0; i < h; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < w; j++) {
+                arr[0][i][j] = Integer.parseInt(st.nextToken());
+                for (int l = 1; l < k + 1; l++) arr[l][i][j] = arr[0][i][j];
+            }
+        }
+        
+        q = new ArrayDeque<>();
+        int[] horse = {2, -2};
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        q.offer(new int[] {0, 0, 0});
+        for (int i = 0; i < k + 1; i++) arr[i][0][0] = 1;
+        while (!q.isEmpty()) {
+            int[] p = q.poll();
+            if (p[0] == h - 1 && p[1] == w - 1) break;
+            if (p[2] < k) {
+                for (int i = 0; i < 2; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        int nx = p[0] + (dx[j] == 0? horse[i]: dx[j]);
+                        int ny = p[1] + (dy[j] == 0? horse[i]: dy[j]);
+                        add(nx, ny, p[2] + 1, arr[p[2]][p[0]][p[1]] + 1);
+                    }
+                }
+            }
+            for (int i = 0; i < 4; i++) {
+                int nx = p[0] + dx[i];
+                int ny = p[1] + dy[i];
+                add(nx, ny, p[2], arr[p[2]][p[0]][p[1]] + 1);
+            }
+        }
+        int answer = Integer.MAX_VALUE;
+        for (int i = 0; i < k + 1; i++) {
+            if (arr[i][h - 1][w - 1] != 0 && answer > arr[i][h - 1][w - 1]) answer = arr[i][h - 1][w - 1];
+        }
+        if (answer == Integer.MAX_VALUE) answer = 0;
+        System.out.println(--answer);
+    }
+    
+    static void add(int x, int y, int num, int step) {
+        if (x >= 0 && y >= 0 && x < h && y < w && arr[num][x][y] == 0) {
+            arr[num][x][y] = step;
+            q.offer(new int[] {x, y, num});
+        }
+    }
+}
