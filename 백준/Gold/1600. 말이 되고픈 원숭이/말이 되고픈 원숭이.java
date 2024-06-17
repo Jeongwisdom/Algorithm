2246456bd7f99b0a4deaa2f84112f8532cf0e218
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-18 07:22:25 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 556 ms, Memory: 63200 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 class Main {
     static int k, w, h;
-    static int[][][] arr;
+    static int[][] arr;
+    static boolean[][][] ch;
     static Deque<int[]> q;
     
     public static void main(String[] args) throws IOException {
@@ -12,11 +13,11 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         w = Integer.parseInt(st.nextToken());
         h = Integer.parseInt(st.nextToken());
-        arr = new int[k + 1][h][w];
+        arr = new int[h][w];
         for (int i = 0; i < h; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < w; j++) {
-                arr[0][i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
         
@@ -24,39 +25,39 @@ class Main {
     }
     
     static int BFS() {
+        ch = new boolean[k + 1][h][w];
         q = new ArrayDeque<>();
         int[] hx = {2, 2, -2, -2, -1, -1, 1, 1};
         int[] hy = {1, -1, 1, -1, 2, -2, 2, -2};
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        q.offer(new int[] {0, 0, 0});
-        arr[0][0][0] = 1;
+        q.offer(new int[] {0, 0, 0, 0});
+        ch[0][0][0] = true;
         while (!q.isEmpty()) {
             int[] p = q.poll();
-            int step = arr[p[2]][p[0]][p[1]];
             if (p[0] == h - 1 && p[1] == w - 1) {
-                return step - 1;
+                return p[3];
             }
             if (p[2] < k) {
                 for (int i = 0; i < 8; i++) {
                     int nx = p[0] + hx[i];
                     int ny = p[1] + hy[i];
-                    add(nx, ny, p[2] + 1, step + 1);
+                    add(nx, ny, p[2] + 1, p[3] + 1);
                 }
             }
             for (int i = 0; i < 4; i++) {
                 int nx = p[0] + dx[i];
                 int ny = p[1] + dy[i];
-                add(nx, ny, p[2], step + 1);
+                add(nx, ny, p[2], p[3] + 1);
             }
         }
         return -1;
     }
     
     static void add(int x, int y, int num, int step) {
-        if (x >= 0 && y >= 0 && x < h && y < w && arr[0][x][y] == 0 && arr[num][x][y] == 0) {
-            arr[num][x][y] = step;
-            q.offer(new int[] {x, y, num});
+        if (x >= 0 && y >= 0 && x < h && y < w && arr[x][y] == 0 && !ch[num][x][y]) {
+            ch[num][x][y] = true;
+            q.offer(new int[] {x, y, num, step});
         }
     }
 }
