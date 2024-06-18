1fdb39bfbca509f0bde06f41855d3f5f92365aa4
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-18 15:22:05 +0900

    [Gold II] Title: 퍼즐, Time: 828 ms, Memory: 117428 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -4,56 +4,44 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
-        Deque<int[]> q = new ArrayDeque<>();
+        StringBuilder start = new StringBuilder();
         StringTokenizer st;
-        for (int i = 0, id = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                arr[id++] = Integer.parseInt(st.nextToken());
+                start.append(st.nextToken());
             }
         }
 
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        int[] original = {1, 2, 3, 4, 5, 6, 7, 8, 0};
-        Set<String> ch = new HashSet<>();
-        q.offer(arr);
-        ch.add(Arrays.toString(arr));
-        int answer = 0;
-        loop:
+        String end = "123456780";
+        Deque<String> q = new ArrayDeque<>();
+        Map<String, Integer> map = new HashMap<>();
+        q.offer(start.toString());
+        map.put(start.toString(), 0);
         while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                int[] tmp = q.poll();
-                if (Arrays.toString(tmp).equals(Arrays.toString(original))) {
-                    q.offer(new int[] {0});
-                    break loop;
-                }
-                for (int k = 0; k < 9; k++) {
-                    if (tmp[k] == 0) {
-                        for (int j = 0; j < 4; j++) {
-                            int nx = k / 3 + dx[j];
-                            int ny = k % 3 + dy[j];
-                            int num = nx * 3 + ny;
-                            if (nx >= 0 && ny >= 0 && nx < 3 && ny < 3) {
-                                int[] t = tmp.clone();
-                                int a = t[k];
-                                t[k] = t[num];
-                                t[num] = a;
-                                if (!ch.contains(Arrays.toString(t))) {
-                                    q.offer(t);
-                                    ch.add(Arrays.toString(t));
-                                }
-                            }
-                        }
-                        break;
+            String tmp = q.poll();
+            if (tmp.equals(end)) break;
+            int zero = tmp.indexOf('0');
+            int x = zero / 3;
+            int y = zero % 3;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                int num = nx * 3 + ny;
+                if (nx >= 0 && ny >= 0 && nx < 3 && ny < 3) {
+                    StringBuilder sb = new StringBuilder(tmp);
+                    char c = sb.charAt(num);
+                    sb.setCharAt(num, '0');
+                    sb.setCharAt(zero, c);
+                    if (!map.containsKey(sb.toString())) {
+                        q.offer(sb.toString());
+                        map.put(sb.toString(), map.get(tmp) + 1);
                     }
                 }
             }
-            answer++;
         }
-        if (q.isEmpty()) answer = -1;
-        System.out.println(answer);
+        System.out.println(map.getOrDefault(end, -1));
     }
 }
