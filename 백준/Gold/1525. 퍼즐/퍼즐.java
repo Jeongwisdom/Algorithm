cd7abca253fd2cc67fce0333e88bce19c48f1ce7
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-18 16:02:10 +0900

    [Gold II] Title: 퍼즐, Time: 496 ms, Memory: 68236 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -4,44 +4,55 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder start = new StringBuilder();
+        int[] arr = new int[10];
         StringTokenizer st;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0, id = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 3; j++) {
-                start.append(st.nextToken());
+            for (int j = 0; j < 3; j++, id++) {
+                arr[id] = Integer.parseInt(st.nextToken());
+                if (arr[id] == 0) arr[9] = id;
             }
         }
-
+        
+        Map<Integer, Integer> ch = new HashMap<>();
+        Deque<int[]> q = new ArrayDeque<>();
+        q.offer(arr);
+        ch.put(parseInt(arr), 0);
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        String end = "123456780";
-        Deque<String> q = new ArrayDeque<>();
-        Map<String, Integer> map = new HashMap<>();
-        q.offer(start.toString());
-        map.put(start.toString(), 0);
+        int end = 123456780;
         while (!q.isEmpty()) {
-            String tmp = q.poll();
-            if (tmp.equals(end)) break;
-            int zero = tmp.indexOf('0');
-            int x = zero / 3;
-            int y = zero % 3;
+            int[] a = q.poll();
+            int changeA = parseInt(a);
+            if (changeA == end) break;
+            int x = a[9] / 3;
+            int y = a[9] % 3;
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                int num = nx * 3 + ny;
                 if (nx >= 0 && ny >= 0 && nx < 3 && ny < 3) {
-                    StringBuilder sb = new StringBuilder(tmp);
-                    char c = sb.charAt(num);
-                    sb.setCharAt(num, '0');
-                    sb.setCharAt(zero, c);
-                    if (!map.containsKey(sb.toString())) {
-                        q.offer(sb.toString());
-                        map.put(sb.toString(), map.get(tmp) + 1);
+                    int num = nx * 3 + ny;
+                    int[] b = a.clone();
+                    int tmp = b[num];
+                    b[num] = 0;
+                    b[b[9]] = tmp;
+                    int change = parseInt(b);
+                    if (!ch.containsKey(change)) {
+                        b[9] = num;
+                        q.offer(b);
+                        ch.put(change, ch.get(changeA) + 1);
                     }
                 }
             }
         }
-        System.out.println(map.getOrDefault(end, -1));
+        System.out.println(ch.getOrDefault(end, -1));
+    }
+    
+    static int parseInt(int[] arr) {
+        int c = 0;
+        for (int i = 0; i < 9; i++) {
+            c = c * 10 + arr[i];
+        }
+        return c;
     }
 }
