e5a6792153fde476e7b05401b2c8b6eec180b4ed
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-20 18:49:18 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 108 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -1,15 +1,22 @@
 import java.io.*;
 
 class Main {
+    static int n;
+    static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 1; i < n; i++) {
-            sb.append("*".repeat(i)).append(" ".repeat((n - i) * 2)).append("*".repeat(i)).append("\n");
+        n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            draw(i);
         }
-        StringBuilder answer = new StringBuilder();
-        answer.append(sb).append("*".repeat(n * 2)).append(sb.reverse());
-        System.out.println(answer);
+        for (int i = n - 1; i >= 1; i--) {
+            draw(i);
+        }
+        System.out.println(sb);
+    }
+
+    static void draw(int i) {
+        sb.append("*".repeat(i)).append(" ".repeat((n - i) * 2)).append("*".repeat(i)).append("\n");
     }
 }
