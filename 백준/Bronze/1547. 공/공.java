a0396f65cfbf0aca947d5205b6575268f2612ae3
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-24 22:37:39 +0900

    [Bronze III] Title: 공, Time: 128 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.java
+++ 백준/Bronze/1547. 공/공.java
@@ -1,16 +1,16 @@
-import java.io.*;
-import java.util.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int m = Integer.parseInt(br.readLine());
-        StringTokenizer st;
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int m = read();
         int a, b, answer = 1;
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            a = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
+            a = read();
+            b = read();
             if (answer == a) answer = b;
             else if (answer == b) answer = a;
         }
