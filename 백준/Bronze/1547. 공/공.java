97962ba0683fdc4805e26d458f2cdf0c25a2385a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-24 22:35:04 +0900

    [Bronze III] Title: 공, Time: 136 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int m = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int a, b, answer = 1;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            if (answer == a) answer = b;
+            else if (answer == b) answer = a;
+        }
+        System.out.println(answer);
+    }
+}
