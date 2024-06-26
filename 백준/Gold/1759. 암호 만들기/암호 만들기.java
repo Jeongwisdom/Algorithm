3315b7e052242c97c8da9fa5dcc66ebf032ada03
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-06-26 16:20:20 +0900

    [Gold V] Title: 암호 만들기, Time: 124 ms, Memory: 14944 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static StringBuilder sb = new StringBuilder();
+    static char[] arr;
+    static int[] idx;
+    static int l, c;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        l = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        arr = new char[c];
+        for (int i = 0; i < c; i++) {
+            arr[i] = st.nextToken().charAt(0);
+        }
+        Arrays.sort(arr);
+        idx = new int[l];
+        DFS(0, 0, 0, 0);
+        System.out.println(sb);
+    }
+
+    static void DFS(int count, int id, int consonant, int vowel) {
+        if (count == l) {
+            if (consonant >= 2 && vowel >= 1) {
+                for (int i = 0; i < l; i++) {
+                    sb.append(arr[idx[i]]);
+                }
+                sb.append("\n");
+            }
+        } else if (id < c) {
+            idx[count] = id;
+            if (arr[id] == 'a' || arr[id] == 'e' || arr[id] == 'o' || arr[id] == 'i' || arr[id] == 'u') DFS(count + 1, id + 1, consonant, vowel + 1);
+            else DFS(count + 1, id + 1, consonant + 1, vowel);
+            DFS(count, id + 1, consonant, vowel);
+        }
+    }
+}
