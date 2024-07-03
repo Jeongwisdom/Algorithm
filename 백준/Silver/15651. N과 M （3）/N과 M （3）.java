ef583f068415d9233ffec450d96f83cb5ee29873
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-03 19:37:01 +0900

    [Silver III] Title: N과 M (3), Time: 432 ms, Memory: 67900 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,20 +1,21 @@
+import java.io.*;
+
 class Main {
     static int n, m;
+    static int[] arr;
     static StringBuilder sb = new StringBuilder();
     
-    static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
-    }
-    public static void main(String[] args) throws Exception {
-        n = read();
-        m = read();
-        DFS(new int[m], 0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        n = Integer.parseInt(split[0]);
+        m = Integer.parseInt(split[1]);
+        arr = new int[m];
+        DFS(0);
         System.out.println(sb);
     }
     
-    static void DFS(int[] arr, int count) {
+    static void DFS(int count) {
         if (count == m) {
             for (int i = 0; i < m; i++) {
                 sb.append(arr[i]).append(" ");
@@ -23,7 +24,7 @@ class Main {
         } else {
             for (int i = 1; i <= n; i++) {
                 arr[count] = i;
-                DFS(arr, count + 1);
+                DFS(count + 1);
             }
         }
     }
