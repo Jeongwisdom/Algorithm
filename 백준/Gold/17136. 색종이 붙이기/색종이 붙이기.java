f63957626335dca71b6c68a7fbf9a694384ae648
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-09 22:08:36 +0900

    [Gold II] Title: 색종이 붙이기, Time: 392 ms, Memory: 24464 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -4,10 +4,11 @@ import java.util.*;
 class Main {
     static int answer = Integer.MAX_VALUE;
     static int[] confetti = new int[5];
+    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[][] arr = new int[10][10];
+        arr = new int[10][10];
         StringTokenizer st;
         for (int i = 0; i < 10; i++) {
             st = new StringTokenizer(br.readLine());
@@ -16,12 +17,12 @@ class Main {
             }
         }
         for (int i = 0; i < 5; i++) confetti[i] = 5;
-        DFS(arr, 0, 0, 0);
+        DFS(0, 0, 0);
         if (answer == Integer.MAX_VALUE) answer = -1;
         System.out.println(answer);
     }
 
-    static int getSquareSize(int[][] arr, int x, int y) {
+    static int getSquareSize(int x, int y) {
         int num = 1;
         while (num <= 4) {
             for (int i = 0; i <= num; i++) {
@@ -33,43 +34,43 @@ class Main {
         return num - 1;
     }
 
-    static void DFS(int[][] arr, int x, int y, int count) {
+    static void DFS(int x, int y, int count) {
+        if (answer == count) return;
         if (x == 9 && y == 10) {
-            if (answer > count) answer = count;
+            answer = count;
             return;
         }
-        if (answer == count) return;
         if (y == 10) {
             x += 1;
             y = 0;
         }
         if (arr[x][y] == 1) {
-            int[][] clone = clone(arr);
-            int num = -1;
-            int size = getSquareSize(arr, x, y);
-            while (num++ < size) {
-                attach(clone , num, x, y);
-                if (confetti[num] > 0) confetti[num]--;
-                else continue;
-                DFS(clone, x, y + 1,count + 1);
-                confetti[num]++;
+            int size = getSquareSize(x, y);
+            attach(size, x, y);
+            while (size >= 0) {
+                if (confetti[size] > 0) {
+                    confetti[size]--;
+                    DFS(x, y + 1,count + 1);
+                    confetti[size]++;
+                }
+                detach(size, x, y);
+                size--;
             }
-        } else DFS(arr, x, y + 1, count);
+        } else DFS(x, y + 1, count);
     }
 
-    static int[][] clone(int[][] arr) {
-        int[][] clone = new int[10][10];
-        for (int i = 0; i < 10; i++) {
-            System.arraycopy(arr[i], 0, clone[i], 0, 10);
-        }
-        return clone;
-    }
-
-    static void attach(int[][] clone, int num, int x, int y) {
+    static void attach(int num, int x, int y) {
         for (int i = 0; i <= num; i++) {
             for (int j = 0; j <= num; j++) {
-                clone[x + i][y + j] = 0;
+                arr[x + i][y + j] = 0;
             }
+        }
+    }
+    
+    static void detach(int num, int x, int y) {
+        for (int i = 0; i <= num; i++) {
+            arr[x + num][y + i] = 1;
+            arr[x + i][y + num] = 1;
         }
     }
 }
