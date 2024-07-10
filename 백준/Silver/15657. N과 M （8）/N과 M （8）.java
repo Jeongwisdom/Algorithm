0dcc4db701a69c9e8cf78fec9aae1c5d3b229e08
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-10 18:14:26 +0900

    [Silver III] Title: N과 M (8), Time: 160 ms, Memory: 19812 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.java
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -5,7 +5,7 @@ class Main {
     static int n, m;
     static int[] number, arr;
     static StringBuilder sb = new StringBuilder();
-    
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,11 +15,11 @@ class Main {
         number = new int[n];
         for (int i = 0; i < n; i++) number[i] = Integer.parseInt(st.nextToken());
         arr = new int[m];
-        Arrays.sort(number);
+        quickSort(0, n - 1);
         DFS(0, 0);
         System.out.println(sb);
     }
-    
+
     static void DFS(int id, int count) {
         if (count == m) {
             for (int i = 0; i < m; i++) sb.append(arr[i]).append(" ");
@@ -30,5 +30,27 @@ class Main {
             arr[count] = number[i];
             DFS(i, count + 1);
         }
+    }
+
+    static void quickSort(int l, int r) {
+        int pivot = partition(l, r);
+        if (l < pivot - 1) quickSort(l, pivot - 1);
+        if (pivot < r) quickSort(pivot, r);
+    }
+
+    static int partition(int l, int r) {
+        int mid = number[(l + r) / 2];
+        while (l <= r) {
+            while (number[l] < mid) l++;
+            while (mid < number[r]) r--;
+            if (l <= r) {
+                int tmp = number[l];
+                number[l] = number[r];
+                number[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+        return l;
     }
 }
