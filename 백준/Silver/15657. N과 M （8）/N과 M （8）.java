9e409e6e1341106bf7d9993b17bb66b316feac95
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-10 18:04:21 +0900

    [Silver III] Title: N과 M (8), Time: 176 ms, Memory: 18044 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int n, m;
+    static int[] number, arr;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        number = new int[n];
+        for (int i = 0; i < n; i++) number[i] = Integer.parseInt(st.nextToken());
+        arr = new int[m];
+        Arrays.sort(number);
+        DFS(0, 0);
+        System.out.println(sb);
+    }
+    
+    static void DFS(int id, int count) {
+        if (count == m) {
+            for (int i = 0; i < m; i++) sb.append(arr[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for (int i = id; i < n; i++) {
+            arr[count] = number[i];
+            DFS(i, count + 1);
+        }
+    }
+}
