21e06d4d4e56ed4680afd755d38d8a31232d151d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-15 11:12:58 +0900

    [Silver II] Title: N과 M (12), Time: 124 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.java
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -15,7 +15,7 @@ class Main {
         number = new int[m];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
+        quickSort(0, n - 1);
         DFS(0, 0);
         System.out.println(sb);
     }
@@ -33,5 +33,27 @@ class Main {
             number[count] = arr[i];
             DFS(i, count + 1);
         }
+    }
+    
+    static void quickSort(int l, int r) {
+        int pivot = partition(l, r);
+        if (l < pivot - 1) quickSort(l, pivot - 1);
+        if (pivot < r) quickSort(pivot, r);
+    }
+    
+    static int partition(int l, int r) {
+        int mid = arr[(l + r) / 2];
+        while (l <= r) {
+            while (arr[l] < mid) l++;
+            while (mid < arr[r]) r--;
+            if (l <= r) {
+                int tmp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+        return l;
     }
 }
