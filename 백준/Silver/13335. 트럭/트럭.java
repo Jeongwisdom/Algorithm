bf5e187a60c2f7c8bd64f27a7c7d9dfcaeeae27c
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-16 21:01:46 +0900

    [Silver I] Title: 트럭, Time: 144 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Silver/13335. 트럭/트럭.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int l = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(st.nextToken());
+        int time = 0;
+        int sum = 0;
+        int id = 0;
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 1; i < w; i++) dq.offer(0);
+        while (id < n) {
+            time++;
+            if (dq.size() == w) {
+                sum -= dq.poll();
+            }
+            if (sum + arr[id] <= l) {
+                sum += arr[id];
+                dq.offer(arr[id++]);
+            } else dq.offer(0);
+        }
+        time += dq.size();
+        System.out.println(time);
+    }
+}
