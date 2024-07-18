95b576d31aa90068e172b87d83b959f326a07a94
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-18 19:23:10 +0900

    [Gold V] Title: 톱니바퀴, Time: 136 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int[][] arr = new int[5][8];
+    static int[] firstId = new int[5];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 1; i < 5; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < 8; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int idx, direction;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            idx = Integer.parseInt(st.nextToken());
+            direction = Integer.parseInt(st.nextToken());
+            rotateLeft(idx, direction);
+            rotateRight(idx, direction);
+            rotate(idx, direction);
+        }
+
+        int answer = 0;
+        for (int i = 1, plus = 1; i < 5; i++, plus *= 2) {
+            if (arr[i][firstId[i]] == 1) answer += plus;
+        }
+        System.out.println(answer);
+    }
+
+    static void rotate(int idx, int direction) {
+        if (direction == 1) {
+            firstId[idx] = (firstId[idx] + 7) % 8;
+        } else {
+            firstId[idx] = (firstId[idx] + 1) % 8;
+        }
+    }
+
+    static void rotateLeft(int idx, int direction) {
+        if (idx == 1) return;
+        int id = (firstId[idx] + 6) % 8;
+        int next = (firstId[idx - 1] + 2) % 8;
+        if (arr[idx][id] != arr[idx - 1][next]) {
+            int dir = direction * -1;
+            rotateLeft(idx - 1, dir);
+            rotate(idx - 1, dir);
+        }
+    }
+
+    static void rotateRight(int idx, int direction) {
+        if (idx == 4) return;
+        int id = (firstId[idx] + 2) % 8;
+        int next = (firstId[idx + 1] + 6) % 8;
+        if (arr[idx][id] != arr[idx + 1][next]) {
+            int dir = direction * -1;
+            rotateRight(idx + 1, dir);
+            rotate(idx + 1, dir);
+        }
+    }
+}
