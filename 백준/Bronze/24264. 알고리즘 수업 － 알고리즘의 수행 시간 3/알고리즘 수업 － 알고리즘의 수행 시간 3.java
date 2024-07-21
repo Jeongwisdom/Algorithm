3661e36bf03f282770ac0d86c8b30083bf3fa203
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-21 17:14:10 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -1,7 +1,9 @@
 class Main {
     public static void main(String[] args) throws Exception {
-        long c, n = System.in.read() & 15;
+        int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
-        System.out.println(n * n + "\n" + (n == 0? 0: 2));
+        long sum = (long) n * n;
+        System.out.println(sum);
+        System.out.println(2);
     }
 }
