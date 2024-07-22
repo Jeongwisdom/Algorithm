5a186028406a998e7d5328bc5019982e4cf61af1
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-07-22 11:38:22 +0900

    [Silver V] Title: Pizza voting, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/10280. Pizza voting/Pizza voting.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int s = 1;
+        int e = n;
+        for (int i = 0; i < n - 1; i++) {
+            if (i % 3 == 0) e--;
+            else if (i % 3 == 1) s++;
+            else {
+                if (m - s >= e - m) {
+                    s++;
+                } else {
+                    e--;
+                }
+            }
+            if (s > e) break;
+        }
+        System.out.println((s == m && e == m)? "YES": "NO");
+    }
+}
