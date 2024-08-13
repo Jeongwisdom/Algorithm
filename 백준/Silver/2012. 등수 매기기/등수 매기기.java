7554803802cf00432bfb3ac68db4f46c3ac4ef7d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-13 23:13:51 +0900

    [Silver III] Title: 등수 매기기, Time: 436 ms, Memory: 44808 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -1,20 +1,43 @@
 import java.io.*;
-import java.util.Arrays;
 
 class Main {
+    static int[] arr;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
+        arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
+        quickSort(0, n - 1);
         
         long answer = 0;
         for (int i = 0; i < n; i++) {
             answer += Math.abs(arr[i] - (i + 1));
         }
         System.out.println(answer);
+    }
+    
+    static void quickSort(int l, int r) {
+        int part = partition(l, r);
+        if (l < part - 1) quickSort(l, part - 1);
+        if (part < r) quickSort(part, r);
+    }
+    
+    static int partition(int l, int r) {
+        int mid = arr[(l + r) / 2];
+        while (l <= r) {
+            while (arr[l] < mid) l++;
+            while (mid < arr[r]) r--;
+            if (l <= r) {
+                int tmp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+        return l;
     }
 }
