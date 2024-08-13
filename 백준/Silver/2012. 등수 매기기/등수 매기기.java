9b3a88564b4e7d1e9d651c63d24763e4d05effa5
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-13 23:16:30 +0900

    [Silver III] Title: 등수 매기기, Time: 336 ms, Memory: 19776 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.java
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -3,12 +3,16 @@ import java.io.*;
 class Main {
     static int[] arr;
     
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c =System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    public static void main(String[] args) throws Exception {
+        int n = read();
         arr = new int[n];
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = read();
         }
         quickSort(0, n - 1);
         
