8fa91343ad703ad370353eba5918fd9af197500a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-15 14:54:41 +0900

    [Gold III] Title: 소문난 칠공주, Time: 236 ms, Memory: 25296 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -5,27 +5,28 @@ class Main {
     static String[] arr = new String[5];
     static boolean[][] ch = new boolean[5][5];
     static Set<String> set = new HashSet<>();
-    static int[][] princess = new int[7][2];
+    static TreeSet<int[]> princess = new TreeSet<>((s1, s2) -> {
+        if (s1[0] == s2[0]) {
+            return s1[1] - s2[1];
+        }
+        return s1[0] - s2[0];
+    });
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
     static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 2; j++) {
-                princess[i][j] = -1;
-            }
-        }
         for (int i = 0; i < 5; i++) arr[i] = br.readLine();
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 ch[i][j] = true;
-                princess[0][0] = i;
-                princess[0][1] = j;
+                int[] p = new int[] {i, j};
+                princess.add(p);
                 if (arr[i].charAt(j) == 'S') DFS(1, 1);
                 else DFS(1, 0);
                 ch[i][j] = false;
+                princess.remove(p);
             }
         }
         System.out.println(answer);
@@ -37,49 +38,30 @@ class Main {
             answer++;
             return;
         }
-        for (int i = 0; i <= cnt; i++) {
+        for (int[] p: new HashSet<>(princess)) {
             for (int j = 0; j < 4; j++) {
-                int nx = princess[i][0] + dx[j];
-                int ny = princess[i][1] + dy[j];
+                int nx = p[0] + dx[j];
+                int ny = p[1] + dy[j];
                 if (nx >= 0 && ny >= 0 && nx < 5 && ny < 5 && !ch[nx][ny]) {
                     ch[nx][ny] = true;
-                    princess[cnt][0] = nx;
-                    princess[cnt][1] = ny;
-                    String str = makeString(nx, ny);
+                    princess.add(new int[] {nx, ny});
+                    String str = makeString();
                     if (!set.contains(str)) {
                         set.add(str);
                         if (arr[nx].charAt(ny) == 'S') DFS(cnt + 1, numOfS + 1);
                         else DFS(cnt + 1, numOfS);
                     }
                     ch[nx][ny] = false;
-                    princess[cnt][0] = -1;
-                    princess[cnt][1] = -1;
+                    princess.removeIf(point -> point[0] == nx && point[1] == ny);
                 }
             }
         }
     }
 
-    static String makeString(int x, int y) {
-        int[][] sort = new int[7][2];
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 2; j++) {
-                sort[i][j] = princess[i][j];
-            }
-        }
-        sort[6][0] = x;
-        sort[6][1] = y;
-        Arrays.sort(sort, (s1, s2) -> {
-            if (s1[0] == s2[0]) {
-                return s1[1] - s2[1];
-            }
-            return s1[0] - s2[0];
-        });
-
+    static String makeString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 2; j++) {
-                sb.append(sort[i][j]);
-            }
+        for (int[] p: princess) {
+            sb.append(p[0]).append(p[1]);
         }
         return sb.toString();
     }
