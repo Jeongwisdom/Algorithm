ce4c619f1cb7fc42d5d1698243a46fc5b115086c
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-17 15:07:14 +0900

    [Gold V] Title: 순열의 순서, Time: 96 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Gold/1722. 순열의 순서/순열의 순서.java
+++ 백준/Gold/1722. 순열의 순서/순열의 순서.java
@@ -1,17 +1,21 @@
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static long read() throws Exception {
+        long c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        boolean[] ch = new boolean[n + 1];
-        String[] split = br.readLine().split(" ");
-        int cmd = Integer.parseInt(split[0]);
-        long fac = factorial(n - 1);
-        int num = n - 1;
+        long n = read();
+        boolean[] ch = new boolean[(int) n + 1];
+        long cmd = read();
+        long fac = factorial((int) n - 1);
+        long num = n - 1;
         if (cmd == 1) {
-            long k = Long.parseLong(split[1]) - 1;
+            long k = read() - 1;
             while (k > 0) {
                 long order = k / fac;
                 for (int i = 1; i <= n; i++) {
@@ -36,7 +40,7 @@ class Main {
         } else {
             long answer = 1;
             for (int i = 0; i < n - 1; i++) {
-                int v = Integer.parseInt(split[i + 1]);
+                long v = read();
                 int figure = 0;
                 for (int j = 1; j <= n; j++) {
                     if (j == v) {
