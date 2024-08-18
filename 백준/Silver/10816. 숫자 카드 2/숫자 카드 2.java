f773609152dfbd920d34737e8855cb9750caa9be
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-18 21:00:37 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 464 ms, Memory: 108196 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,20 +1,28 @@
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    static int read() throws Exception {
+        int c, n = System.in.read();
+        boolean minus = false;
+        if (n < 48) {
+            minus = true;
+            n = 0;
+        } else n &= 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return minus? -1 * n: n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int n = read();
         int buffer = 10000000;
         int[] arr = new int[2 * buffer + 1];
-        String[] split = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            arr[Integer.parseInt(split[i]) + buffer]++;
+            arr[read() + buffer]++;
         }
         StringBuilder sb = new StringBuilder();
-        int m = Integer.parseInt(br.readLine());
-        split = br.readLine().split(" ");
+        int m = read();
         for (int i = 0; i < m; i++) {
-            sb.append(arr[Integer.parseInt(split[i]) + buffer]).append(" ");
+            sb.append(arr[read() + buffer]).append(" ");
         }
         System.out.println(sb);
     }
