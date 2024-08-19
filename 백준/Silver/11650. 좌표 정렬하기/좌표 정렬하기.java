04147eed0ec509e9553396cfa9600a9af7a65f12
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-19 18:52:59 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 456 ms, Memory: 27944 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -2,15 +2,23 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+    static int read() throws Exception {
+        int c, n = System.in.read();
+        boolean minus = false;
+        if (n < 48) {
+            minus = true;
+            n = 0;
+        } else n &= 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return minus? -1 * n: n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int n = read();
         int[][] arr = new int[n][2];
-        StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][0] = read();
+            arr[i][1] = read();
         }
         Arrays.sort(arr, (a1, a2) -> {
             if (a1[0] == a2[0]) return a1[1] - a2[1];
