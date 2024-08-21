bb4838cb9342a3704e37f175fcffe8c8cc8a6829
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-21 22:02:02 +0900

    [Bronze I] Title: 경우의 수의 합, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/30189. 경우의 수의 합/경우의 수의 합.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+        System.out.println((n + 1) * (m + 1));
+    }
+}
