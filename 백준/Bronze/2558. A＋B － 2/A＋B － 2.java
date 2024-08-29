74c81af6ce34588cc8594dbc24780638c333d1cf
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-08-30 03:54:08 +0900

    [Bronze V] Title: A+B - 2, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int a = read();
+        int b = read();
+        System.out.println(a + b);
+    } 
+}
