0405f319aa7782aec1af81308f4dc48e7ceadd51
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-09-09 03:20:02 +0900

    [Silver III] Title: 걷기, Time: 96 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/1459. 걷기/걷기.java
+++ 백준/Silver/1459. 걷기/걷기.java
@@ -1,20 +1,22 @@
-import java.io.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        long x = Integer.parseInt(split[0]);
-        long y = Integer.parseInt(split[1]);
-        long w = Integer.parseInt(split[2]);
-        long s = Integer.parseInt(split[3]);
+    static long read() throws Exception {
+        long c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        long x = read();
+        long y = read();
+        long w = read();
+        long s = read();
         long min = x < y? x: y;
         long max = x < y? y: x;
         long answer;
         if (2 * w > s) {
             answer = min * s;
             if (w > s) answer += (max - min) / 2 * 2 * s + (max - min) % 2 * w;
-            else answer += (max -min) * w;
+            else answer += (max - min) * w;
         } else answer = w * (x + y);
         System.out.println(answer);
     }
