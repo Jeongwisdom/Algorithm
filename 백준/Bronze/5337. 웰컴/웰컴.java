8c1b3edf9964e9fa75974de05d9b9f0f9355e6ec
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-09-10 22:44:20 +0900

    [Bronze V] Title: 웰컴, Time: 92 ms, Memory: 13812 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n").append("|  | _ | _. _ ._ _  _\n").append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}
