3dfe96330113dfee2219ec0b4546fd756c61dbc7
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-09-13 01:39:39 +0900

    [Bronze V] Title: 아!, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/4999. 아！/아！.java
+++ 백준/Bronze/4999. 아！/아！.java
@@ -3,11 +3,9 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s1 = br.readLine();
-        String s2 = br.readLine();
-        String answer;
-        if (s1.length() >= s2.length()) answer = "go";
-        else answer = "no";
-        System.out.println(answer);
+        int s1 = br.readLine().length();
+        int s2 = br.readLine().length();
+        if (s1 >= s2) System.out.println("go");
+        else System.out.println("no");
     }
 }
