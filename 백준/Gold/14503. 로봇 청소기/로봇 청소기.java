2b0e977f5d0de4508148d300ede8cdd0c06d45ee
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-09-20 23:00:10 +0900

    [Gold V] Title: 로봇 청소기, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,23 +1,22 @@
-import java.io.*;
-import java.util.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        int r = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        int m = read();
+        int r = read();
+        int c = read();
+        int d = read();
         int[] dx = {-1, 0, 1, 0};
         int[] dy = {0, 1, 0, -1};
         int[][] arr = new int[n][m];
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+                arr[i][j] = read();
             }
         }
 
