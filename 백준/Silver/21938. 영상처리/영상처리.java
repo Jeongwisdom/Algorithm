df728ec7842a132eea09373d35d725585faecf48
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-09-21 23:24:01 +0900

    [Silver II] Title: 영상처리, Time: 244 ms, Memory: 27880 KB -BaekjoonHub

--- 백준/Silver/21938. 영상처리/영상처리.java
+++ 백준/Silver/21938. 영상처리/영상처리.java
@@ -1,6 +1,9 @@
-import java.util.*;
-
 class Main {
+    static int[][] arr;
+    static int n, m, t, nx, ny, answer = 0;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
@@ -8,44 +11,37 @@ class Main {
     }
     
     public static void main(String[] args) throws Exception {
-        int n = read();
-        int m = read();
-        int[][] arr = new int[n][m];
+        n = read();
+        m = read();
+        arr = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 arr[i][j] = (read() + read() + read()) / 3;
             }
         }
-        int t = read();
+        t = read();
         
-        boolean[][] ch = new boolean[n][m];
-        int[] dx = {0, 0, -1, 1};
-        int[] dy = {1, -1, 0, 0};
-        int answer = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!ch[i][j]) {
-                    ch[i][j] = true;
-                    if (arr[i][j] >= t) {
-                        answer++;
-                        Deque<int[]> q = new ArrayDeque<>();
-                        q.offer(new int[] {i, j});
-                        while (!q.isEmpty()) {
-                            int[] p = q.poll();
-                            for (int k = 0; k < 4; k++) {
-                                int nx = p[0] + dx[k];
-                                int ny = p[1] + dy[k];
-                                if (nx < 0 || ny < 0 || nx >= n || ny >= m || ch[nx][ny]) continue;
-                                ch[nx][ny] = true;
-                                if (arr[nx][ny] >= t) {
-                                    q.offer(new int[] {nx, ny});
-                                }
-                            }
-                        }
-                    }
+                if (arr[i][j] >= t) {
+                    answer++;
+                    arr[i][j] = -1;
+                    DFS(i, j);
                 }
             }
         }
         System.out.println(answer);
+    }
+    
+    static void DFS(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if (arr[nx][ny] >= t) {
+                arr[nx][ny] = -1;
+                DFS(nx, ny);
+            }
+        }
     }
 }
