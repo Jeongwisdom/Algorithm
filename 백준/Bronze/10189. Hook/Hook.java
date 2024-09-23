5a31ad9933f4e8659a88b0060bfa64e8e6a55e61
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-09-23 23:34:50 +0900

    [Bronze V] Title: Hook, Time: 92 ms, Memory: 13876 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("#  # #### #### #  #\n").append("#### #  # #  # # #\n").append("#### #  # #  # # #\n").append("#  # #### #### #  #");
+        System.out.println(sb);
+    }
+}
