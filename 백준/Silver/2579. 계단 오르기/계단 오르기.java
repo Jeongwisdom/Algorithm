3427dc8352d2c8824368b02340731c616c235687
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-09-28 22:53:07 +0900

    [Silver III] Title: 계단 오르기, Time: 96 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -7,40 +7,17 @@ class Main {
 
     public static void main(String[] args) throws Exception {
         int n = read();
-        int[] stair = new int[n];
-        for (int i = 0; i < n; i++) {
+        int[] stair = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
             stair[i] = read();
         }
 
-        int[][] dp = new int[n][2];
-        dp[0][0] = stair[0];
-        dp[0][1] = 1;
-        if (n >= 2) {
-            dp[1][0] = stair[0] + stair[1];
-            dp[1][1] = 2;
+        int[] dp = new int[n + 1];
+        dp[1] = stair[1];
+        if (n >= 2) dp[2] = dp[1] + stair[2];
+        for (int i = 3; i <= n; i++) {
+            dp[i] = stair[i] + Math.max(dp[i - 2], dp[i - 3] + stair[i - 1]);
         }
-        if (n >= 3) {
-            if (stair[0] + stair[2] < stair[1] + stair[2]) {
-                dp[2][0] = stair[1] + stair[2];
-                dp[2][1] = 2;
-            } else {
-                dp[2][0] = stair[0] + stair[2];
-                dp[2][1] = 1;
-            }
-        }
-        for (int i = 3; i < n; i++) {
-            if (dp[i - 1][1] < 2) {
-                dp[i][0] = dp[i - 1][0] + stair[i];
-                dp[i][1] = dp[i - 1][1] + 1;
-            } else {
-                dp[i][0] = dp[i - 3][0] + stair[i - 1] + stair[i];
-                dp[i][1] = 2;
-            }
-            if (dp[i][0] <= dp[i - 2][0] + stair[i]) {
-                dp[i][0] = dp[i - 2][0] + stair[i];
-                dp[i][1] = 1;
-            }
-        }
-        System.out.println(dp[n - 1][0]);
+        System.out.println(dp[n]);
     }
 }
