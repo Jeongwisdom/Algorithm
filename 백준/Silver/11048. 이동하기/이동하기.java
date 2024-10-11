effedb5c029a9d4d5f1e5f241fd65e9a1cd043c4
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-11 19:48:39 +0900

    [Silver II] Title: 이동하기, Time: 152 ms, Memory: 18696 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -11,7 +11,7 @@ class Main {
         int[][] arr = new int[n + 1][m + 1];
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                arr[i][j] = Math.max(arr[i - 1][j], arr[i][j - 1]) + read();
+                arr[i][j] = Math.max(arr[i - 1][j - 1], Math.max(arr[i - 1][j], arr[i][j - 1])) + read();
             }
         }
         System.out.println(arr[n][m]);
