845dc2451d2d12da65a8841f510533fe889660d3
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-12 17:00:57 +0900

    [Gold IV] Title: 타일 채우기, Time: 100 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -8,7 +8,7 @@ class Main {
         } else {
             int[] dp = new int[n + 1];
             dp[0] = 1;
-            if (n >= 2) dp[2] = 3;
+            dp[2] = 3;
             for (int i = 4; i <= n; i += 2) {
                 dp[i] = dp[i - 2] * 4 - dp[i - 4];
             }
