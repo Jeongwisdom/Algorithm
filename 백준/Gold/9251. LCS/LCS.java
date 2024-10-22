d58d8bc7025ed238c913a3b1787b3e845d26965f
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-22 19:55:35 +0900

    [Gold V] Title: LCS, Time: 124 ms, Memory: 18384 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,20 +1,24 @@
-import java.io.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str1 = br.readLine();
-        String str2 = br.readLine();
-        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+    public static void main(String[] args) throws Exception {
+        char[] arr1 = read();
+        char[] arr2 = read();
+        int[][] dp = new int[arr1.length][arr2.length];
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[i].length; j++) {
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                if (arr1[i] == arr2[j]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-        System.out.println(dp[str1.length()][str2.length()]);
+        System.out.println(dp[arr1.length - 1][arr2.length - 1]);
+    }
+
+    static char[] read() throws Exception {
+        StringBuilder sb = new StringBuilder("a");
+        int c;
+        while ((c = System.in.read()) > 64) sb.append((char)c);
+        return sb.toString().toCharArray();
     }
 }
