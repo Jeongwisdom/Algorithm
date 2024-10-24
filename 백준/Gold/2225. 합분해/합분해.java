867b55cc6c3f6e5aba0655b7ef43fef0d90a05e5
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-24 16:50:49 +0900

    [Gold V] Title: 합분해, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -8,14 +8,15 @@ class Main {
     public static void main(String[] args) throws Exception {
         int n = read();
         int k = read();
-        int[][] dp = new int[n + 1][k + 1];
-        for (int i = 1; i <= k; i++) dp[0][i] = 1;
+        int[][] dp = new int[n][k];
+        for (int i = 0; i < k; i++) dp[0][i] = i + 1;
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = 1;
+            for (int j = 1; j < k; j++) {
                 dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000;
             }
         }
-        System.out.println(dp[n][k]);
+        System.out.println(dp[n - 1][k - 1]);
     }
 }
