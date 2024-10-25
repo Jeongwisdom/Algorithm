1d5b2b05eaacc0d7b2b720343f662187abb3c394
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-25 15:36:50 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 564 ms, Memory: 66556 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -1,36 +1,36 @@
+import java.io.*;
+
 class Main {
-    static int n, answer = 1;
+    static int n;
     static int[][] bamboo, dp;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {1, -1, 0, 0};
 
-    static int read() throws Exception {
-        int r, c = System.in.read() & 15;
-        while ((r = System.in.read()) > 47) c = (c << 3) + (c << 1) + (r & 15);
-        return c;
-    }
-
-    public static void main(String[] args) throws Exception {
-        n = read();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
         bamboo = new int[n][n];
+        String[] split;
         for (int i = 0; i < n; i++) {
+            split = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                bamboo[i][j] = read();
+                bamboo[i][j] = Integer.parseInt(split[j]);
             }
         }
 
         dp = new int[n][n];
+        int answer = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                DFS(i, j);
+                answer = Math.max(answer, DFS(i, j));
             }
         }
         System.out.println(answer);
     }
 
     static int DFS(int x, int y) {
-        if (dp[x][y] != 0) return dp[x][y];
-        dp[x][y]++;
+        if (dp[x][y] > 0) return dp[x][y];
+        dp[x][y] = 1;
         int nx, ny;
         for (int i = 0; i < 4; i++) {
             nx = x + dx[i];
@@ -38,7 +38,6 @@ class Main {
             if (nx < 0 || ny < 0 || nx >= n || ny >= n) continue;
             if (bamboo[x][y] < bamboo[nx][ny]) dp[x][y] = Math.max(dp[x][y], DFS(nx, ny) + 1);
         }
-        if (answer < dp[x][y]) answer = dp[x][y];
         return dp[x][y];
     }
 }
