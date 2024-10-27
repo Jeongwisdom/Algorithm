d479b73f08726646f30ae98f3562814424adf3b0
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-27 21:55:34 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 264 ms, Memory: 23484 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -6,15 +6,13 @@ class Main {
         String[] split = br.readLine().split(" ");
         int n = Integer.parseInt(split[0]);
         int m = Integer.parseInt(split[1]);
-        int[][] arr = new int[n + 1][m + 1];
         int[][] dp = new int[n + 1][m + 1];
         int answer = 0;
         String str;
         for (int i = 1; i <= n; i++) {
             str = br.readLine();
             for (int j = 1; j <= m; j++) {
-                arr[i][j] = str.charAt(j - 1) - '0';
-                if (arr[i][j] == 1) {
+                if (str.charAt(j - 1) == '1') {
                     dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                     if (answer < dp[i][j]) answer = dp[i][j];
                 }
