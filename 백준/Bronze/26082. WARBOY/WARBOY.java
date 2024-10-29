24151183cefc588ed2fbbee1f41ff630f52c4db5
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-29 23:43:51 +0900

    [Bronze V] Title: WARBOY, Time: 104 ms, Memory: 14048 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,15 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int a = read();
+        int b = read();
+        int c = read();
+        int answer = b / a * 3 * c;
+        System.out.println(answer);
+    }
+}
