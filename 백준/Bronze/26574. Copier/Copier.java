4179ed3ebf0bf81c7b20d5795ac5d3c2336f4976
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-10-31 23:42:38 +0900

    [Bronze V] Title: Copier, Time: 100 ms, Memory: 13956 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            sb.append(num).append(" ").append(num).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
