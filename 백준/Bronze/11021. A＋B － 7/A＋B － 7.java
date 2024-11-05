86a49e62e680ea0428e13556159103ccd5e2a3bd
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-11-05 22:12:04 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,19 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int n = read();
+        StringBuilder sb = new StringBuilder();
+        int a, b;
+        for (int i = 1; i <= n; i++) {
+            a = read();
+            b = read();
+            sb.append("Case #").append(i).append(": ").append(a + b).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
