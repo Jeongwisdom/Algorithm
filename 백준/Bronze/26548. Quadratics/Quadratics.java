e1274cec0960277e36d09cfa65e429103e11889f
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-11-14 20:57:40 +0900

    [Bronze III] Title: Quadratics, Time: 108 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Bronze/26548. Quadratics/Quadratics.java
+++ 백준/Bronze/26548. Quadratics/Quadratics.java
@@ -6,14 +6,16 @@ class Main {
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         String[] split;
-        double a, b, c, d;
+        double a, b, c, d, e, f;
         for (int i = 0; i < n; i++) {
             split = br.readLine().split(" ");
             a = Double.parseDouble(split[0]);
             b = Double.parseDouble(split[1]);
             c = Double.parseDouble(split[2]);
-            d = Math.sqrt(b * b - 4 * a * c);
-            sb.append(String.format("%.3f", (-1 * b + d) / (2 * a))).append(", ").append(String.format("%.3f", (-1 * b - d) / (2 * a))).append("\n");
+            d = -1 * b;
+            e = Math.sqrt(b * b - 4 * a * c);
+            f = 2 * a;
+            sb.append(String.format("%.3f", (d + e) / f)).append(", ").append(String.format("%.3f", (d - e) / f)).append("\n");
         }
         System.out.println(sb);
     }
