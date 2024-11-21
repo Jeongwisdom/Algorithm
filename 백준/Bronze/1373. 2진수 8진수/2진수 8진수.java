b7b5fc06d6abc63543c9725fd86ff5416b12cf3f
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-11-22 00:14:16 +0900

    [Bronze I] Title: 2진수 8진수, Time: 276 ms, Memory: 21696 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -1,10 +1,22 @@
 import java.io.*;
-import java.math.BigInteger;
 
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger i = new BigInteger(br.readLine(), 2);
-        System.out.println(i.toString(8));
+        String str = br.readLine();
+        StringBuilder sb = new StringBuilder();
+        int num;
+        if (str.length() % 3 == 1) {
+            num = str.charAt(0) & 15;
+            sb.append(num);
+        } else if (str.length() % 3 == 2) {
+            num = (str.charAt(0) & 15) * 2 + (str.charAt(1) & 15);
+            sb.append(num);
+        }
+        for (int i = str.length() % 3; i < str.length(); i += 3) {
+            num = (str.charAt(i) & 15) * 4 + (str.charAt(i + 1) & 15) * 2 + (str.charAt(i + 2) & 15);
+            sb.append(num);
+        }
+        System.out.println(sb);
     }
 }
