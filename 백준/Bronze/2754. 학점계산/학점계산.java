a65915837295e7b77b8547d23f53d146b814ff8d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-11-27 21:33:05 +0900

    [Bronze III] Title: 학점계산, Time: 108 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String grade = br.readLine();
+        String answer;
+        switch (grade) {
+            case "A+":
+                answer = "4.3";
+                break;
+            case "A0":
+                answer = "4.0";
+                break;
+            case "A-":
+                answer = "3.7";
+                break;
+            case "B+":
+                answer = "3.3";
+                break;
+            case "B0":
+                answer = "3.0";
+                break;
+            case "B-":
+                answer = "2.7";
+                break;
+            case "C+":
+                answer = "2.3";
+                break;
+            case "C0":
+                answer = "2.0";
+                break;
+            case "C-":
+                answer = "1.7";
+                break;
+            case "D+":
+                answer = "1.3";
+                break;
+            case "D0":
+                answer = "1.0";
+                break;
+            case "D-":
+                answer = "0.7";
+                break;
+            default:
+                answer = "0.0";
+                break;
+        }
+        System.out.println(answer);
+    }
+}
