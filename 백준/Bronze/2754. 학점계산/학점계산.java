1b48c982a9464336f5040a9801d62e3e35ecba0f
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-11-27 21:33:28 +0900

    [Bronze III] Title: 학점계산, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

--- 백준/Bronze/2754. 학점계산/학점계산.java
+++ 백준/Bronze/2754. 학점계산/학점계산.java
@@ -5,46 +5,32 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String grade = br.readLine();
         String answer;
-        switch (grade) {
-            case "A+":
-                answer = "4.3";
-                break;
-            case "A0":
-                answer = "4.0";
-                break;
-            case "A-":
-                answer = "3.7";
-                break;
-            case "B+":
-                answer = "3.3";
-                break;
-            case "B0":
-                answer = "3.0";
-                break;
-            case "B-":
-                answer = "2.7";
-                break;
-            case "C+":
-                answer = "2.3";
-                break;
-            case "C0":
-                answer = "2.0";
-                break;
-            case "C-":
-                answer = "1.7";
-                break;
-            case "D+":
-                answer = "1.3";
-                break;
-            case "D0":
-                answer = "1.0";
-                break;
-            case "D-":
-                answer = "0.7";
-                break;
-            default:
-                answer = "0.0";
-                break;
+        if (grade.equals("A+")) {
+            answer = "4.3";
+        } else if (grade.equals("A0")) {
+            answer = "4.0";
+        } else if (grade.equals("A-")) {
+            answer = "3.7";
+        } else if (grade.equals("B+")) {
+            answer = "3.3";
+        } else if (grade.equals("B0")) {
+            answer = "3.0";
+        } else if (grade.equals("B-")) {
+            answer = "2.7";
+        } else if (grade.equals("C+")) {
+            answer = "2.3";
+        } else if (grade.equals("C0")) {
+            answer = "2.0";
+        } else if (grade.equals("C-")) {
+            answer = "1.7";
+        } else if (grade.equals("D+")) {
+            answer = "1.3";
+        } else if (grade.equals("D0")) {
+            answer = "1.0";
+        } else if (grade.equals("D-")) {
+            answer = "0.7";
+        } else {
+            answer = "0.0";
         }
         System.out.println(answer);
     }
