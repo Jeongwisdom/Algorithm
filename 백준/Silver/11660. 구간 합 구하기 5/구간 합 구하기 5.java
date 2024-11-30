6f546f93713bf45a56a50830df11c2464596dd14
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-11-30 21:05:45 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 348 ms, Memory: 23304 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -6,26 +6,23 @@ class Main {
     }
     
     public static void main(String[] args) throws Exception {
-        int n = read();
+        int n = read() + 1;
         int m = read();
-        int[][] dp = new int[n + 1][n + 1];
-        int num;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                num = read();
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + num - dp[i - 1][j - 1];
+        int[][] dp = new int[n][n];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = read() + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
             }
         }
         
         StringBuilder sb = new StringBuilder();
-        int x1, y1, x2, y2, sum;
+        int x1, y1, x2, y2;
         for (int i = 0; i < m; i++) {
-            x1 = read();
-            y1 = read();
+            x1 = read() - 1;
+            y1 = read() - 1;
             x2 = read();
             y2 = read();
-            sum = dp[x2][y2] + dp[x1 - 1][y1 - 1] - dp[x2][y1 - 1]  - dp[x1 - 1][y2];
-            sb.append(sum).append("\n");
+            sb.append(dp[x2][y2] + dp[x1][y1] - dp[x2][y1] - dp[x1][y2]).append("\n");
         }
         System.out.println(sb);
     }
