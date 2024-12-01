1132a64e3df9b3fba48dda20ef1c2693768275f5
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-12-01 22:01:46 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 108 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        char[] arr = br.readLine().toCharArray();
+        for (int i = 1; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] != '?' && str.charAt(j) != arr[j]) {
+                    arr[j] = '?';
+                }
+            }
+        }
+        
+        System.out.println(arr);
+    }
+}
