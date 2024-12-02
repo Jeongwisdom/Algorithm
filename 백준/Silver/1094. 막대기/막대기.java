cccb60df5d675f38fdea9542081a34309205dda4
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-12-02 20:15:57 +0900

    [Silver V] Title: 막대기, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,11 +1,11 @@
 class Main {
     public static void main(String[] args) throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        int r, n = System.in.read() & 15;
+        while ((r = System.in.read()) > 47) n = (n << 3) + (n << 1) + (r & 15);
         String bi = Integer.toBinaryString(n);
         int answer = 0;
-        for (int i = 0; i < bi.length(); i++) {
-            if (bi.charAt(i) == '1') answer++;
+        for (char c: bi.toCharArray()) {
+            if (c == '1') answer++;
         }
         System.out.println(answer);
     }
