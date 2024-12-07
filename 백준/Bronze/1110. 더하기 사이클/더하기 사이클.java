cf8ab6e56617f773c15294cfa2729ee3cf269021
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-12-07 23:31:33 +0900

    [Bronze I] Title: 더하기 사이클, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,21 +1,16 @@
 class Main {
-    static int read() throws Exception {
+    public static void main(String[] args) throws Exception {
         int r, c = System.in.read() & 15;
         while ((r = System.in.read()) > 47) c = (c << 3) + (c << 1) + (r & 15);
-        return c;
-    }
-    
-    public static void main(String[] args) throws Exception {
-        int n = read();
         int answer = 0;
-        int ex = n;
+        int ex = c;
         int one;
         while (true) {
             one = ex % 10;
             ex /= 10;
             ex = (one * 10) + (one + ex) % 10;
             answer++;
-            if (ex == n) break;
+            if (ex == c) break;
         }
         System.out.println(answer);
     }
