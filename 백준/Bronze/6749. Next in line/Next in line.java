802f7954e57e417f8f0e5009502afc17ae703206
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-12-09 22:47:04 +0900

    [Bronze IV] Title: Next in line, Time: 100 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,14 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int a = read();
+        int b = read();
+        int dif = b - a;
+        System.out.println(b + dif);
+    }
+}
