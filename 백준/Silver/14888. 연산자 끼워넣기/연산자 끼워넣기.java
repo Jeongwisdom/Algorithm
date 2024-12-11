70bbd9f4b4c6436094bcff3112eb8ca2d2e2055f
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2024-12-11 18:44:03 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -0,0 +1,50 @@
+class Main {
+    static int n, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+    static int[] arr, op;
+    
+    static int read() throws Exception {
+        int r, c = System.in.read() & 15;
+        while ((r = System.in.read()) > 47) c = (c << 3) + (c << 1) + (r & 15);
+        return c;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        n = read();
+        arr = new int[n];
+        op = new int[4];
+        for (int i = 0; i < n; i++) {
+            arr[i] = read();
+        }
+        for (int i = 0; i < 4; i++) {
+            op[i] = read();
+        }
+        
+        DFS(1, arr[0]);
+        System.out.println(max);
+        System.out.println(min);
+    }
+    
+    static void DFS(int id, int num) {
+        if (id == n) {
+            if (max < num) max = num;
+            if (min > num) min = num;
+            return;
+        }
+        
+        for (int i = 0; i < 4; i++) {
+            if (op[i] > 0) {
+                int cal = calculate(i, num, arr[id]);
+                op[i]--;
+                DFS(id + 1, cal);
+                op[i]++;
+            }
+        }
+    }
+    
+    static int calculate(int i, int a, int b) {
+        if (i == 0) return a + b;
+        if (i == 1) return a - b;
+        if (i == 2) return a * b;
+        return a / b;
+    }
+}
