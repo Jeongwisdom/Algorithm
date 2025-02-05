a12cbe8e6ac81af1d22f890f24f748434e85fed0
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-02-06 02:00:49 +0900

    [Gold I] Title: 구간 합 구하기, Time: 360 ms, Memory: 41124 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -8,7 +8,7 @@ class Main {
             n = 0;
         }
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
-        return minus? -1 * n: n;
+        return minus? -n: n;
     }
     
     public static void main(String[] args) throws Exception {
@@ -17,22 +17,53 @@ class Main {
         for (int i = 1; i <= n; i++) {
             arr[i] = read();
         }
+        SegmentTree tree = new SegmentTree(n);
+        tree.init(arr, 1, 1, n);
         
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m + k; i++) {
             int a = (int) read(), b = (int) read();
             long c = read();
             if (a == 1) {
+                long diff = c - arr[b];
                 arr[b] = c;
+                tree.update(1, 1, n, b, diff);
             } else {
-                long answer = 0;
-                for (int j = b; j <= c; j++) {
-                    answer += arr[j];
-                }
-                sb.append(answer).append("\n");
+                sb.append(tree.sum(1, 1, n, b, (int) c)).append("\n");
             }
         }
         
         System.out.println(sb);
+    }
+}
+
+class SegmentTree {
+    long[] trees;
+    int size;
+    
+    public SegmentTree(int arrSize) {
+        int h = (int) Math.ceil(Math.log(arrSize) / Math.log(2));
+        this.size = 1 << (h + 1);
+        this.trees = new long[size];
+    }
+    
+    public long init(long[] arr, int id, int start, int end) {
+        if (start == end) return trees[id] = arr[start];
+        return trees[id] = init(arr, id * 2, start, (start + end) / 2) + init(arr, id * 2 + 1, (start + end) / 2 + 1, end);
+    }
+    
+    public void update(int id, int start, int end, int updateId, long diff) {
+        if (updateId < start || updateId > end) return;
+        trees[id] += diff;
+        if (start != end) {
+            update(id * 2, start, (start + end) / 2, updateId, diff);
+            update(id * 2 + 1, (start + end) / 2 + 1, end, updateId, diff);
+        }
+    }
+    
+    public long sum(int id, int start, int end, int left, int right) {
+        if (left > end || right < start) return 0;
+        if (left <= start && right >= end) return trees[id];
+        return sum(id * 2, start, (start + end) / 2, left, right) + sum(id * 2 + 1, (start + end) / 2 + 1, end, left, right);
     }
 }
