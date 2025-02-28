ba5eea15d7297c43d935ab9a1bb196ed7db1e629
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-01 06:13:03 +0900

    [Bronze II] Title: 숫자의 개수, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,17 +1,26 @@
-import java.io.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] num = new int[10];
-        int a = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
-        String ans = String.valueOf(a);
-        for (int i = 0; i < ans.length(); i++) {
-            num[ans.charAt(i) - '0']++;
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+    
+    public static void main(String[] args) throws Exception {
+        int a = read();
+        int b = read();
+        int c = read();
+        int multiply = a * b * c;
+        
+        int[] arr = new int[10];
+        if (multiply == 0) arr[0]++;
+        while (multiply > 0) {
+            arr[multiply % 10]++;
+            multiply /= 10;
         }
+        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            sb.append(num[i]).append("\n");
+        for (int i: arr) {
+            sb.append(i).append("\n");
         }
         System.out.println(sb);
     }
