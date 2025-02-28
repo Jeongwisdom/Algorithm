1e65dc4d008de0957e6afc1cd0281f752429e4ac
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-01 08:14:51 +0900

    [Bronze II] Title: Strfry, Time: 280 ms, Memory: 20900 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.java
+++ 백준/Bronze/11328. Strfry/Strfry.java
@@ -5,19 +5,21 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int[][] arr;
+        int[] arr;
         for (int i = 0; i < n; i++) {
-            arr = new int[2][26];
+            arr = new int[26];
             String[] split = br.readLine().split(" ");
-            for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < split[j].length(); k++) {
-                    arr[j][split[j].charAt(k) - 97]++;
-                }
+            for (int j = 0; j < split[0].length(); j++) {
+                arr[split[0].charAt(j) - 97]++;
+            }
+            
+            for (int j = 0; j < split[1].length(); j++) {
+                arr[split[1].charAt(j) - 97]--;
             }
 
             boolean ch = true;
             for (int j = 0; j < 26; j++) {
-                if (arr[0][j] != arr[1][j]) {
+                if (arr[j] != 0) {
                     ch = false;
                     break;
                 }
