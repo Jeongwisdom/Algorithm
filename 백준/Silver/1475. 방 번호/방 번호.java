81e9fcdba89fff5db9df9adab3e33ddd4313de55
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-01 06:31:06 +0900

    [Silver V] Title: 방 번호, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -6,7 +6,7 @@ class Main {
             arr[n & 15]++;
         }
 
-        int answer = (int) Math.ceil((arr[6] + arr[9]) / 2.0);
+        int answer = (arr[6] + arr[9] + 1) / 2;
         for (int i = 0; i < 10; i++) {
             if (i != 6 && i != 9) {
                 if (arr[i] > answer) answer = arr[i];
