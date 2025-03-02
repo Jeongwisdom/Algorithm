16f85d20e8a28367998771062045e4234e58e5d0
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-03 05:36:16 +0900

    [Silver II] Title: 키로거, Time: 416 ms, Memory: 88400 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -1,53 +1,59 @@
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            Node head = new Node();
-            Node now = head;
-            for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                if (c == '<') {
-                    if (now.prev != null) now = now.prev;
-                } else if (c == '>') {
-                    if (now.next != null) now = now.next;
-                } else if (c == '-') {
-                    if (now.prev != null) {
-                        if (now.next != null) now.next.prev = now.prev;
-                        now.prev.next = now.next;
-                        now = now.prev;
-                    }
-                } else {
-                    now = new Node(c, now, now.next);
-                    now.prev.next = now;
-                    if (now.next != null) now.next.prev = now;
+    public static void main(String[] args) throws Exception {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+
+        Node head, current, node;
+        while (n-- > 0) {
+            head = new Node();
+            current = head;
+            while ((c = System.in.read()) > 44) {
+                switch(c) {
+                    case 60:
+                        if (current.pre == null) break;
+                        current = current.pre;
+                        break;
+                    case 62:
+                        if (current.next == null) break;
+                        current = current.next;
+                        break;
+                    case 45:
+                        if (current.pre == null) break;
+                        current.pre.next = current.next;
+                        if (current.next != null) current.next.pre = current.pre;
+                        current = current.pre;
+                        break;
+                    default:
+                        node = new Node((char) c, current);
+                        node.next = current.next;
+                        if (current.next != null) current.next.pre = node;
+                        current.next = node;
+                        current = node;
                 }
             }
-            now = head;
-            while (now.next != null) {
-                now = now.next;
-                sb.append(now.value);
+
+            head = head.next;
+            while (head != null) {
+                bw.write(head.data);
+                head = head.next;
             }
-            sb.append("\n");
+            bw.write("\n");
         }
-        System.out.println(sb);
+        bw.flush();
     }
 }
 
 class Node {
-    char value = 0;
-    Node prev = null;
-    Node next = null;
-    
+    char data;
+    Node pre, next;
+
     Node() {}
-    
-    Node(char value, Node prev, Node next) {
-        this.value = value;
-        this.prev = prev;
-        this.next = next;
+
+    Node(char data, Node pre) {
+        this.data = data;
+        this.pre = pre;
     }
 }
