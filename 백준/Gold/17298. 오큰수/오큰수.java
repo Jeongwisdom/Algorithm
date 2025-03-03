f6a1d486c7d9ec4a7b86346a61d35b14f7dc0f1d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-04 00:05:06 +0900

    [Gold IV] Title: 오큰수, Time: 560 ms, Memory: 66188 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+class Main {
+    static int c, n;
+
+    static int read() throws Exception {
+        n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int N = read();
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) numbers[i] = read();
+
+        int[] stack = new int[N];
+        int[] answer = new int[N];
+        int id = -1;
+        for (int i = N - 1; i >= 0; i--) {
+            while (id >= 0) {
+                if (numbers[i] < stack[id]) break;
+                id--;
+            }
+            answer[i] = id >= 0? stack[id]: -1;
+            stack[++id] = numbers[i];
+        }
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i = 0; i < N; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.write(" ");
+        }
+        bw.flush();
+    }
+}
