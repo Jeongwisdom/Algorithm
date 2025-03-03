226c0e0be67f7b376662c3ce352b26ba228c57b7
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-03 21:17:08 +0900

    [Silver IV] Title: 스택, Time: 128 ms, Memory: 16484 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -10,30 +10,29 @@ class Main {
         String str;
         while (n-- > 0) {
             str = br.readLine();
-            switch (str) {
-                case "top":
+            switch (str.charAt(0)) {
+                case 't':
                     if (size == 0) bw.write("-1");
                     else bw.write(stack[size - 1]);
                     bw.write("\n");
                     break;
-                case "size":
+                case 's':
                     bw.write(String.valueOf(size));
                     bw.write("\n");
                     break;
-                case "empty":
+                case 'e':
                     if (size == 0) bw.write("1\n");
                     else bw.write("0\n");
                     break;
-                case "pop":
-                    if (size == 0) bw.write("-1");
-                    else {
-                        size--;
-                        bw.write(stack[size]);
-                    }
-                    bw.write("\n");
-                    break;
-                default:
-                    stack[size++] = str.substring(5);
+                case 'p':
+                    if (str.charAt(1) == 'o') {
+                        if (size == 0) bw.write("-1");
+                        else {
+                            size--;
+                            bw.write(stack[size]);
+                        }
+                        bw.write("\n");
+                    } else stack[size++] = str.substring(5);
             }
         }
         bw.flush();
