79da6cd0cad1b5ccb658d6db849d6e406cb8b544
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-04 00:29:34 +0900

    [Gold IV] Title: 오큰수, Time: 372 ms, Memory: 38992 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 class Main {
     static int c, n;
 
@@ -12,22 +14,32 @@ class Main {
         int[] numbers = new int[N];
         for (int i = 0; i < N; i++) numbers[i] = read();
 
+        int MAX = 1000000 << 3;
+        char[] buffer = new char[MAX];
         int[] stack = new int[N];
-        int[] answer = new int[N];
-        int id = -1;
+        int num, bufferId = MAX, id = -1;
         for (int i = N - 1; i >= 0; i--) {
             while (id >= 0) {
                 if (numbers[i] < stack[id]) break;
                 id--;
             }
-            answer[i] = id >= 0? stack[id]: -1;
+
+            if (id >= 0) {
+                num = stack[id];
+                while (num > 0) {
+                    buffer[--bufferId] = (char) (num % 10 + 48);
+                    num /= 10;
+                }
+            } else {
+                buffer[--bufferId] = 49;
+                buffer[--bufferId] = 45;
+            }
+            buffer[--bufferId] = 32;
             stack[++id] = numbers[i];
         }
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(answer[i]).append(" ");
-        }
-        System.out.println(sb);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(buffer, ++bufferId, MAX - bufferId);
+        bw.flush();
     }
 }
