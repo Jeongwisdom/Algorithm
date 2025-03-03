ac8a76e19c438b208850014bebb0c7c6f2db3642
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-03 23:53:14 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 132 ms, Memory: 15312 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -1,25 +1,33 @@
 class Main {
     static int c, n;
-    
+
     static int read() throws Exception {
         n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
-    
+
     public static void main(String[] args) throws Exception {
         int N = read();
         int[] buildings = new int[N];
         for (int i = 0; i < N; i++) {
             buildings[i] = read();
         }
-        
+
         long answer = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                if (buildings[i] <= buildings[j]) break;
-                answer++;
+        int[] stack = new int[N];
+        int[] num = new int[N];
+        int see, id = -1;
+        for (int i = N - 1; i >= 0; i--) {
+            see = 0;
+            while (id >= 0) {
+                if (buildings[i] <= stack[id]) break;
+                see += num[id] + 1;
+                id--;
             }
+            stack[++id] = buildings[i];
+            num[id] = see;
+            answer += see;
         }
         System.out.println(answer);
     }
