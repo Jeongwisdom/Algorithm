9520514923719b69a496763cc561895fe032f5e6
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-04 21:44:13 +0900

    [Silver IV] Title: 큐, Time: 128 ms, Memory: 16896 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String[] q = new String[10000];
+        String str;
+        int head = 0, tail = 0;
+        while (n-- > 0) {
+            str = br.readLine();
+            switch (str.charAt(0)) {
+                case 115:
+                    bw.write(String.valueOf(tail - head));
+                    bw.write("\n");
+                    break;
+                case 101:
+                    if (head == tail) bw.write("1\n");
+                    else bw.write("0\n");
+                    break;
+                case 102:
+                    if (head == tail) bw.write("-1");
+                    else bw.write(q[head]);
+                    bw.write("\n");
+                    break;
+                case 98:
+                    if (head == tail) bw.write("-1");
+                    else bw.write(q[tail - 1]);
+                    bw.write("\n");
+                    break;
+                default:
+                    if (str.charAt(1) == 111) {
+                        if (head == tail) bw.write("-1");
+                        else {
+                            bw.write(q[head++]);
+                        }
+                        bw.write("\n");
+                    } else {
+                        q[tail++] = str.substring(5);
+                    }
+            }
+        }
+        bw.flush();
+    }
+}
