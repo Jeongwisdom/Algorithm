7a8d64148e081759c527cd86e3ceeab612bd7c67
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-06 21:28:45 +0900

    [Gold V] Title: AC, Time: 584 ms, Memory: 74764 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,51 +1,53 @@
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int n = Integer.parseInt(br.readLine());
-        for (int c = 0; c < n; c++) {
-            String command = br.readLine();
-            int m = Integer.parseInt(br.readLine());
-            String s = br.readLine();
-            String[] arr = s.substring(1, s.length() - 1).split(",");
-            int h = 0;
-            int t = m - 1;
-            boolean zero = m == 0;
-            boolean ch = true;
-            for (int i = 0; i < command.length(); i++) {
-                if (command.charAt(i) == 'R') {
-                    int tmp = h;
-                    h = t;
-                    t = tmp;
-                } else {
-                    if (zero) {
-                        ch = false;
-                        break;
-                    } else if (h < t) h++;
-                    else if (h > t) h--;
-                    else zero = true;
-                }
+        int head, tail, m, n = Integer.parseInt(br.readLine());
+        String command, s;
+        String[] split;
+        int[] arr;
+        boolean r;
+        loop:
+        while (n-- > 0) {
+            command = br.readLine();
+            m = Integer.parseInt(br.readLine());
+            s = br.readLine();
+            split = s.substring(1, s.length() - 1).split(",");
+            arr = new int[m];
+            for (int i = 0; i < m; i++) {
+                arr[i] = Integer.parseInt(split[i]);
             }
-            if (!ch) sb.append("error\n");
-            else {
-                sb.append("[");
-                if (!zero) {
-                    if (h <= t) {
-                        for (int i = h; i <= t; i++) {
-                            sb.append(arr[i]);
-                            if (i < t) sb.append(",");
-                        }
-                    } else {
-                        for (int i = h; i >= t; i--) {
-                            sb.append(arr[i]);
-                            if (i > t) sb.append(",");
-                        }
+
+            head = 0;
+            tail = m - 1;
+            r = false;
+            for (char c: command.toCharArray()) {
+                if (c == 'R') r = !r;
+                else {
+                    if (head > tail) {
+                        sb.append("error\n");
+                        continue loop;
                     }
+                    if (r) tail--;
+                    else head++;
                 }
-                sb.append("]\n");
             }
+
+            sb.append("[");
+            if (r) {
+                for (int i = tail; i >= head; i--) {
+                    sb.append(arr[i]);
+                    if (i > head) sb.append(",");
+                }
+            } else {
+                for (int i = head; i <= tail; i++) {
+                    sb.append(arr[i]);
+                    if (i < tail) sb.append(",");
+                }
+            }
+            sb.append("]\n");
         }
         System.out.println(sb);
     }
