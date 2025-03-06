4a67ccdde437ba9359679728fb44f9a605bd8953
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-07 06:30:25 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1964 ms, Memory: 629480 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.java
@@ -5,15 +5,21 @@ class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int num, n = Integer.parseInt(st.nextToken()), L = Integer.parseInt(st.nextToken()) - 1;
-        Deque<int[]> q = new ArrayDeque<>();
+        int n = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken()) - 1;
+        
+        int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            num = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        Deque<int[]> q = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
             while (!q.isEmpty() && q.getFirst()[0] < i - L) q.pollFirst();
-            while (!q.isEmpty() && q.getLast()[1] >= num) q.pollLast();
-            q.offer(new int[] {i, num});
+            while (!q.isEmpty() && q.getLast()[1] >= arr[i]) q.pollLast();
+            q.offer(new int[] {i, arr[i]});
             sb.append(q.getFirst()[1]).append(" ");
         }
         System.out.println(sb);
