75609162c95fbc2961106884d116395245d2e62d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-07 08:03:08 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 192 ms, Memory: 18344 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.java
@@ -1,26 +1,23 @@
 class Main {
-    static int c, n, N, id, stackId;
-    static int[] stack, numId, arr;
-    static long answer;
-
     static int read() throws Exception {
-        n = System.in.read() & 15;
+        int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
 
     public static void main(String[] args) throws Exception {
         StringBuilder sb = new StringBuilder();
+        int N;
         while ((N = read()) > 0) {
-            arr = new int[N + 1];
+            int[] arr = new int[N + 1];
             for (int i = 0; i < N; i++) {
                 arr[i] = read();
             }
 
-            stack = new int[N + 1];
-            numId = new int[N + 1];
-            stackId = -1;
-            answer = 0;
+            int[] stack = new int[N + 1];
+            int[] numId = new int[N + 1];
+            int id, stackId = -1;
+            long answer = 0;
             for (int i = 0; i <= N; i++) {
                 id = i;
                 while (stackId >= 0 && stack[stackId] >= arr[i]) {
