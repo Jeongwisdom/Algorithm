70e4cbb5fd5853dba9dd74aad70bb9d4235f1e65
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-10 20:59:07 +0900

    [Silver IV] Title: 좋은 단어, Time: 132 ms, Memory: 21712 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -1,18 +1,30 @@
 class Main {
     public static void main(String[] args) throws Exception {
-        int a, n = System.in.read() & 15;
-        while ((a = System.in.read()) > 32) n = (n << 3) + (n << 1) + (a & 15);
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            char c;
-            char[] s = new char[100000];
-            int id = -1;
-            while ((c = (char) System.in.read()) > 32) {
-                if (id == -1 || s[id] != c) {
-                    s[++id] = c;
-                } else id--;
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+
+        int[] stack;
+        int tail, answer = 0;
+        while (n-- > 0) {
+            stack = new int[100000];
+            tail = 0;
+            while ((c = System.in.read()) > 64) {
+                if (c == 65) {
+                    if (tail == 0 || stack[tail - 1] == 2) {
+                        stack[tail++] = 1;
+                    } else {
+                        tail--;
+                    }
+                }
+                else {
+                    if (tail == 0 || stack[tail - 1] == 1) {
+                        stack[tail++] = 2;
+                    } else {
+                        tail--;
+                    }
+                }
             }
-            if (id == -1) answer++;
+            if (tail == 0) answer++;
         }
         System.out.println(answer);
     }
