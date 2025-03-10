99bf7895a3afeb6e73d77ff03c6fb827112460b9
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-10 21:08:19 +0900

    [Silver IV] Title: 괄호, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,30 +1,29 @@
 import java.io.*;
-import java.util.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        Stack<Character> s;
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            s = new Stack<>();
-            if (str.length() % 2 == 1) s.push('(');
-            else {
-                for (int j = 0; j < str.length(); j++) {
-                    if (str.charAt(j) == '(') {
-                        s.push('(');
+
+        StringBuilder sb = new StringBuilder();
+        String str;
+        int tail;
+        while (n-- > 0) {
+            str = br.readLine();
+            tail = 0;
+            for (char c: str.toCharArray()) {
+                if (c == '(') {
+                    tail++;
+                } else {
+                    if (tail == 0) {
+                        tail++;
+                        break;
                     } else {
-                        if (s.isEmpty()) {
-                            s.push('(');
-                            break;
-                        }
-                        s.pop();
+                        tail--;
                     }
                 }
             }
-            if (s.isEmpty()) sb.append("YES\n");
+            if (tail == 0) sb.append("YES\n");
             else sb.append("NO\n");
         }
         System.out.println(sb);
