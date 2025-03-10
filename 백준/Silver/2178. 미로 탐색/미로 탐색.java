6e03c68f7fc51b83f90b6767aa714bbfc52502e9
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-11 03:15:34 +0900

    [Silver I] Title: 미로 탐색, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,16 +1,12 @@
-import java.io.*;
-import java.util.*;
-
 class Main {
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
-    
+
     public static void main(String[] args) throws Exception {
-        int n = read();
-        int m = read();
+        int n = read(), m = read();
         int[][] arr = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -18,31 +14,33 @@ class Main {
             }
             System.in.read();
         }
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] ch = new boolean[n][m];
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        q.offer(new int[] {0, 0});
-        ch[0][0] = true;
-        int answer = 0;
+
+        int NM = n * m;
+        int[] qx = new int[NM];
+        int[] qy = new int[NM];
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+        int head = 0, tail = 1;
+        arr[0][0] = 0;
+        int nx, ny, len, cnt = 0;
         loop:
-        while (!q.isEmpty()) {
-            answer++;
-            Queue<int[]> e = new LinkedList<>();
-            while (!q.isEmpty()) {
-                int[] point = q.poll();
-                if (point[0] == n - 1 && point[1] == m - 1) break loop;
-                for (int i = 0; i < 4; i++) {
-                    int nx = point[0] + dx[i];
-                    int ny = point[1] + dy[i];
-                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 1 && !ch[nx][ny]) {
-                        ch[nx][ny] = true;
-                        e.offer(new int[] {nx, ny});
-                    }
+        while (head < tail) {
+            cnt++;
+            len = tail - head;
+            for (int i = 0; i < len; i++) {
+                for (int j = 0; j < 4; j++) {
+                    nx = qx[head] + dx[j];
+                    ny = qy[head] + dy[j];
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                    if (arr[nx][ny] == 0) continue;
+                    if (nx == n - 1 && ny == m - 1) break loop;
+                    arr[nx][ny] = 0;
+                    qx[tail] = nx;
+                    qy[tail++] = ny;
                 }
+                head++;
             }
-            q = e;
         }
-        System.out.println(answer);
+        System.out.println(++cnt);
     }
 }
