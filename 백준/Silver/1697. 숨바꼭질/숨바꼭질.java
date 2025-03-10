cd83ae4a8844203777392611c1a136df68e9740d
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-11 04:43:35 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 16540 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,40 +1,36 @@
-import java.util.*;
-
 class Main {
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
-    
+
     public static void main(String[] args) throws Exception {
-        int n = read();
-        int k = read();
-        int max = Math.max(n, k);
-        boolean[] ch = new boolean[max * 2 + 1];
-        int[] arr = new int[max * 2 + 1];
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(k);
-        ch[k] = true;
-        while (!q.isEmpty()) {
-            int num = q.poll();
-            if (num == n) break;
-            if (num < ch.length - 1 && !ch[num + 1]) {
-                ch[num + 1] = true;
-                arr[num + 1] = arr[num] + 1;
-                q.offer(num + 1);
-            }
-            if (num >= 1 && !ch[num - 1]) {
-                ch[num - 1] = true;
-                arr[num - 1] = arr[num] + 1;
-                q.offer(num - 1);
-            }
-            if (num % 2 == 0 && !ch[num / 2]) {
-                ch[num / 2] = true;
-                arr[num / 2] = arr[num] + 1;
-                q.offer(num / 2);
-            }
+        int N = read(), K = read();
+        int[] dist = new int[150000];
+        int[] q = new int[150000];
+        dist[N] = 1;
+        q[0] = N;
+        int head = 0, tail = 1;
+        int[] num = new int[3];
+        if (N == K) {
+            System.out.println(0);
+            return;
         }
-        System.out.println(arr[n]);
+        loop:
+        while (head < tail) {
+            num[0] = q[head] + 1;
+            num[1] = q[head] - 1;
+            num[2] = q[head] * 2;
+            for (int i = 0; i < 3; i++) {
+                if (num[i] < 0 || num[i] >= 150000) continue;
+                if (dist[num[i]]> 0) continue;
+                dist[num[i]] = dist[q[head]] + 1;
+                if (num[i] == K) break loop;
+                q[tail++] = num[i];
+            }
+            head++;
+        }
+        System.out.println(dist[K] - 1);
     }
 }
