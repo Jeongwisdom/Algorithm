30e462ee3b0fe7162b03352e7d3947b4d52888a1
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-11 03:36:59 +0900

    [Gold V] Title: 토마토, Time: 216 ms, Memory: 26876 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,46 +1,56 @@
-import java.util.*;
-
 class Main {
     static int read() throws Exception {
-        int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
-        return n;
+        boolean minus = false;
+        int c, n = System.in.read();
+        if (n == 45) {
+            minus = true;
+            n = System.in.read();
+        }
+        n = n & 15;
+        while ((c =  System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return minus? -n: n;
     }
-    
+
     public static void main(String[] args) throws Exception {
-        int m = read();
-        int n = read();
+        int m = read(), n = read();
         int[][] arr = new int[n][m];
-        Deque<int[]> q = new ArrayDeque<>();
-        int tomato = 0;
+        int NM = n * m;
+        int[] qx = new int[NM];
+        int[] qy = new int[NM];
+        int tomato = 0, head = 0, tail = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 arr[i][j] = read();
-                if (arr[i][j] == 1) q.offer(new int[] {i, j});
-                else if (arr[i][j] == 0) tomato++;
-            }
-        }
-        int answer = 0;
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        while (!q.isEmpty()) {
-            if (tomato == 0) break;
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                int[] point = q.poll();
-                for (int j = 0; j < 4; j++) {
-                    int nx = point[0] + dx[j];
-                    int ny = point[1] + dy[j];
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < m && arr[nx][ny] == 0) {
-                        arr[nx][ny] = 1;
-                        tomato--;
-                        q.offer(new int[] {nx, ny});
-                    }
+                if (arr[i][j] == 0) tomato++;
+                else if (arr[i][j] == 1) {
+                    qx[tail] = i;
+                    qy[tail++] = j;
                 }
             }
-            answer++;
         }
-        if (tomato != 0) answer = -1;
-        System.out.println(answer);
+
+        int nx, ny, len, cnt = 0;
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, 1, -1};
+        while (head < tail) {
+            if (tomato == 0) break;
+            cnt++;
+            len = tail - head;
+            while (len-- > 0) {
+                for (int i = 0; i < 4; i++) {
+                    nx = qx[head] + dx[i];
+                    ny = qy[head] + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+                    if (arr[nx][ny] != 0) continue;
+                    arr[nx][ny] = 1;
+                    qx[tail] = nx;
+                    qy[tail++] = ny;
+                    tomato--;
+                }
+                head++;
+            }
+        }
+        if (tomato > 0) cnt = -1;
+        System.out.println(cnt);
     }
 }
