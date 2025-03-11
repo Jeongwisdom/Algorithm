c07873fa5b21639c4fede3e404fabdcd784f88fa
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-12 02:45:40 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 352 ms, Memory: 36120 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,20 +1,18 @@
 class Main {
-    static int cnt = 0;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        sb.append((1 << n) - 1).append('\n');
         hanoi(n, 1, 3);
-        System.out.println(cnt);
         System.out.println(sb);
     }
 
     static void hanoi(int n, int a, int b) {
         if (n == 0) return;
-        cnt++;
         hanoi(n - 1, a, 6 - a - b);
-        sb.append(a).append(" ").append(b).append("\n");
+        sb.append(a).append(' ').append(b).append('\n');
         hanoi(n - 1, 6 - a - b, b);
     }
 }
