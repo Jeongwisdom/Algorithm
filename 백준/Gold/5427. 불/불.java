a427f27b70ccc58362562a798c04ad914a9145d5
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-12 01:21:22 +0900

    [Gold IV] Title: 불, Time: 428 ms, Memory: 44504 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,56 +1,76 @@
-import java.io.*;
-import java.util.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int w, h, fhead, ftail, head, tail, cnt, len, nx, ny, T = read();
+        int[][] arr;
+        int[] fx = new int[1000000], fy = new int[1000000], qx = new int[1000000], qy = new int[1000000];
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
         StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-        StringTokenizer st;
-        for (int c = 0; c < t; c++) {
-            st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int h = Integer.parseInt(st.nextToken());
-            char[][] arr = new char[h][w];
-            Queue<int[]> q = new ArrayDeque<>();
-            int[] s = new int[3];
+        while (T-- > 0) {
+            w = read();
+            h = read();
+            fhead = 0;
+            ftail = 0;
+            head = 0;
+            tail = 0;
+            arr = new int[h][w];
             for (int i = 0; i < h; i++) {
-                String str = br.readLine();
                 for (int j = 0; j < w; j++) {
-                    arr[i][j] = str.charAt(j);
-                    if (arr[i][j] == '*') q.offer(new int[] {i, j});
-                    else if (arr[i][j] == '@') s = new int[] {i, j};
-                }
-            }
-            q.offer(s);
-            int answer = 0;
-            loop:
-            while (!q.isEmpty()) {
-                int len = q.size();
-                answer++;
-                for (int i = 0; i < len; i++) {
-                    int[] point = q.poll();
-                    for (int j = 0; j < 4; j++) {
-                        int nx = point[0] + dx[j];
-                        int ny = point[1] + dy[j];
-                        if (nx < 0 || ny < 0 || nx >= h || ny >= w) {
-                            if (arr[point[0]][point[1]] == '@') {
-                                q.offer(new int[] {0});
-                                break loop;
-                            }
-                        } else {
-                            if (arr[nx][ny] == '.') {
-                                arr[nx][ny] = arr[point[0]][point[1]];
-                                q.offer(new int[] {nx, ny});
-                            }
-                        }
+                    arr[i][j] = System.in.read();
+                    if (arr[i][j] == '@') {
+                        qx[tail] = i;
+                        qy[tail++] = j;
+                    } else if (arr[i][j] == '*') {
+                        fx[ftail] = i;
+                        fy[ftail++] = j;
                     }
                 }
+                System.in.read();
             }
-            if (q.isEmpty()) sb.append("IMPOSSIBLE\n");
-            else sb.append(answer).append("\n");
+
+            cnt = 0;
+            loop:
+            while (head < tail) {
+                cnt++;
+                len = ftail - fhead;
+                while (len-- > 0) {
+                    for (int i = 0; i < 4; i++) {
+                        nx = fx[fhead] + dx[i];
+                        ny = fy[fhead] + dy[i];
+                        if (nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+                        if (arr[nx][ny] != '.') continue;
+                        arr[nx][ny] = '*';
+                        fx[ftail] = nx;
+                        fy[ftail++] = ny;
+                    }
+                    fhead++;
+                }
+
+                len = tail - head;
+                while (len-- > 0) {
+                    for (int i = 0; i < 4; i++) {
+                        nx = qx[head] + dx[i];
+                        ny = qy[head] + dy[i];
+                        if (nx < 0 || ny < 0 || nx >= h || ny >= w) {
+                            tail++;
+                            break loop;
+                        }
+                        if (arr[nx][ny] != '.') continue;
+                        arr[nx][ny] = '@';
+                        qx[tail] = nx;
+                        qy[tail++] = ny;
+                    }
+                    head++;
+                }
+            }
+            if (head == tail) sb.append("IMPOSSIBLE\n");
+            else sb.append(cnt).append("\n");
         }
         System.out.println(sb);
     }
