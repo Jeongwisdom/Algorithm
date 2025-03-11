93ee31947f6424ffbd40387e4fe52e569d9bc071
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-12 01:52:42 +0900

    [Silver I] Title: 곱셈, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -1,22 +1,20 @@
-import java.util.*;
-import java.io.*;
-
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-
-        System.out.println(divide(a, b, c));
+    static long read() throws Exception {
+        long c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return n;
     }
     
-    public static long divide(int a, int b, int c) {
-        if (b == 0) return 1;
-        long answer = divide(a, b / 2, c);
-        answer = answer * answer % c;
-        if (b % 2 == 1) answer = answer * a % c;
-        return answer;
+    public static void main(String[] args) throws Exception {
+        long a = read(), b = read(), c = read();
+        System.out.println(rec(a, b, c));
+    }
+    
+    static long rec(long a, long b, long c) {
+        if (b == 1) return a % c;
+        long val = rec(a, b / 2, c);
+        val = val * val % c;
+        if (b % 2 == 0) return val;
+        return a * val % c;
     }
 }
