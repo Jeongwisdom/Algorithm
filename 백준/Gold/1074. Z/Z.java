d1c533c4f2139685817125bc9ab78653119e460e
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-12 04:11:27 +0900

    [Gold V] Title: Z, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Gold/1074. Z/Z.java
+++ 백준/Gold/1074. Z/Z.java
@@ -1,30 +1,22 @@
 class Main {
-    static int answer, N, r, c;
-
     static int read() throws Exception {
-        int a, b = System.in.read() & 15;
-        while ((a = System.in.read()) > 47) b = (b << 3) + (b << 1) + (a & 15);
-        return b;
+        int n = 0, a;
+        while ((a = System.in.read()) > 47) n = (n << 3) + (n << 1) + (a & 15);
+        return n;
     }
 
     public static void main(String[] args) throws Exception {
-        N = read();
-        r = read();
-        c = read();
-        int num = 1 << N - 1;
-        z(0, 0, num, 0);
-        System.out.println(answer);
+        int N = read(), r = read(), c = read();
+        System.out.println(z(N, r, c));
     }
 
-    static void z(int x, int y, int num, int cnt) {
-        if (x == r && y == c) {
-            answer = cnt;
-            return;
-        }
-        int sum = num * num;
-        if (x <= r && r < x + num && y <= c && c < y + num) z(x, y, num >> 1, cnt);
-        else if (x <= r && r < x + num && y + num <= c && c < y + num + num) z(x, y + num, num >> 1, cnt + sum);
-        else if (x + num <= r && r < x + num + num && y <= c && c < y + num) z(x + num, y, num >> 1, cnt + sum + sum);
-        else if (x + num <= r && r < x + num + num && y + num <= c && c < y + num + num) z(x + num, y + num, num >> 1, cnt + sum + sum + sum);
+    static int z(int N, int r, int c) {
+        if (N == 0) return 0;
+        int half = 1 << (N - 1);
+
+        if (r < half && c < half) return z(N - 1, r, c);
+        else if (r < half) return half * half + z(N - 1, r, c - half);
+        else if (c < half) return 2 * half * half + z(N - 1, r - half, c);
+        return 3 * half * half + z(N - 1, r - half, c - half);
     }
 }
