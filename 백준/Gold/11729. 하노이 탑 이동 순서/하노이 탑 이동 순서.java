7706961f4a979ca340184bd5e4c3eec12ea7400a
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-12 02:58:59 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 184 ms, Memory: 47936 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,18 +1,29 @@
+import java.io.*;
+
 class Main {
-    static StringBuilder sb = new StringBuilder();
+    static char[] buffer;
+    static int id = 0;
 
     public static void main(String[] args) throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
-        sb.append((1 << n) - 1).append('\n');
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = (1 << n) - 1;
+        buffer = new char[num << 4];
+        bw.write(String.valueOf(num));
+        bw.write('\n');
         hanoi(n, 1, 3);
-        System.out.println(sb);
+        bw.write(buffer, 0, id);
+        bw.flush();
     }
 
     static void hanoi(int n, int a, int b) {
         if (n == 0) return;
         hanoi(n - 1, a, 6 - a - b);
-        sb.append(a).append(' ').append(b).append('\n');
+        buffer[id++] = (char) (a + '0');
+        buffer[id++] = ' ';
+        buffer[id++] = (char) (b + '0');
+        buffer[id++] = '\n';
         hanoi(n - 1, 6 - a - b, b);
     }
 }
