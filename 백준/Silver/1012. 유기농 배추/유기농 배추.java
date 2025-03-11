2bd047684ede32cd13d3bc0c8e7671fde192bc86
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-12 00:04:11 +0900

    [Silver II] Title: 유기농 배추, Time: 108 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,53 +1,52 @@
-import java.util.*;
-import java.io.*;
-
 class Main {
+    static int M, N, K, X, Y, nx, ny;
+    static boolean[][] arr;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
-        while ((c = System.in.read()) > 32) n = (n << 3) + (n << 1) + (c & 15);
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
     
     public static void main(String[] args) throws Exception {
+        int answer, T = read();
         StringBuilder sb = new StringBuilder();
-        int t = read();
-        for (int a = 0; a < t; a++) {
-            int answer = 0;
-            int n = read();
-            int m = read();
-            int k = read();
-            boolean[][] arr = new boolean[n][m];
-            for (int b = 0; b < k; b++) {
-                int x = read();
-                int y = read();
-                arr[x][y] = true;
+        while (T-- > 0) {
+            M = read();
+            N = read();
+            K = read();
+            arr = new boolean[M][N];
+            while (K-- > 0) {
+                X = read();
+                Y = read();
+                arr[X][Y] = true;
             }
-            boolean[][] ch = new boolean[n][m];
-            int[] dx = {-1, 1, 0, 0};
-            int[] dy = {0, 0, -1, 1};
-            Queue<int[]> q = new LinkedList<>();
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    if (arr[i][j] && !ch[i][j]) {
+            
+            answer = 0;
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (arr[i][j]) {
                         answer++;
-                        ch[i][j] = true;
-                        q.offer(new int[] {i, j});
-                        while (!q.isEmpty()) {
-                            int[] point = q.poll();
-                            for (int l = 0; l < 4; l++) {
-                                int nx = point[0] + dx[l];
-                                int ny = point[1] + dy[l];
-                                if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] && !ch[nx][ny]) {
-                                    ch[nx][ny] = true;
-                                    q.offer(new int[] {nx, ny});
-                                }
-                            }
-                        }
+                        arr[i][j]= false;
+                        DFS(i, j);
                     }
                 }
             }
             sb.append(answer).append("\n");
         }
         System.out.println(sb);
+    }
+    
+    static void DFS(int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            nx = x + dx[i];
+            ny = y + dy[i];
+            if (nx < 0 || ny < 0 || nx >= M || ny >= N) continue;
+            if (!arr[nx][ny]) continue;
+            arr[nx][ny] = false;
+            DFS(nx, ny);
+        }
     }
 }
