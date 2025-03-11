b5f21347a456fcaa4a28c4104b5f84ed1cf230d7
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-12 00:58:10 +0900

    [Silver I] Title: 나이트의 이동, Time: 252 ms, Memory: 38180 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,40 +1,48 @@
-import java.util.*;
-
 class Main {
     static int read() throws Exception {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
         return n;
     }
-    
+
     public static void main(String[] args) throws Exception {
+        int L, LL, X, Y, cnt, len, nx, ny, T = read();
+        int[][] arr;
+        int[] qx, qy;
+        int[] dx = {2, 2, -2, -2, 1, -1, 1, -1};
+        int[] dy = {1, -1, 1, -1, 2, 2, -2, -2};
         StringBuilder sb = new StringBuilder();
-        int t = read();
-        int[] dx = {-2, -2, 2, 2, -1, 1, -1, 1};
-        int[] dy = {-1, 1, -1, 1, -2, -2, 2, 2};
-        for (int c = 0; c < t; c++) {
-            int n = read();
-            int sx = read();
-            int sy = read();
-            int ex = read();
-            int ey = read();
-            Queue<int[]> q = new ArrayDeque<>();
-            int[][] ch = new int[n][n];
-            ch[sx][sy] = 1;
-            q.offer(new int[] {sx, sy});
-            while (!q.isEmpty()) {
-                int[] point = q.poll();
-                if (ex == point[0] && ey == point[1]) break;
-                for (int i = 0; i < 8; i++) {
-                    int nx = point[0] + dx[i];
-                    int ny = point[1] + dy[i];
-                    if (nx >= 0 && ny >= 0 && nx < n && ny < n && ch[nx][ny] == 0) {
-                        ch[nx][ny] = ch[point[0]][point[1]] + 1;
-                        q.offer(new int[] {nx, ny});
+        while (T-- > 0) {
+            L = read();
+            arr = new int[L][L];
+            LL = L * L;
+            qx = new int[LL];
+            qy = new int[LL];
+            int head = 0, tail = 0;
+            qx[tail] = read();
+            qy[tail++] = read();
+            X = read();
+            Y = read();
+            cnt = 0;
+            arr[qx[head]][qy[head]] = 1;
+            while (head < tail) {
+                if (arr[X][Y] == 1) break;
+                cnt++;
+                len = tail - head;
+                while (len-- > 0) {
+                    for (int i = 0; i < 8; i++) {
+                        nx = qx[head] + dx[i];
+                        ny = qy[head] + dy[i];
+                        if (nx < 0 || ny < 0 || nx >= L || ny >= L) continue;
+                        if (arr[nx][ny] == 1) continue;
+                        arr[nx][ny] = 1;
+                        qx[tail] = nx;
+                        qy[tail++] = ny;
                     }
+                    head++;
                 }
             }
-            sb.append(ch[ex][ey] - 1).append("\n");
+            sb.append(cnt).append("\n");
         }
         System.out.println(sb);
     }
