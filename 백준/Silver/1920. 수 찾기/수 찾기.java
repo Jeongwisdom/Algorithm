65414422c0fe63554c9fa23d54ef1def84be8827
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-22 05:42:40 +0900

    [Silver IV] Title: 수 찾기, Time: 392 ms, Memory: 20364 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,59 +1,49 @@
 import java.util.*;
-import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+    static int N, M;
+    static int[] arr, sort;
+
+    static int read() throws Exception {
+        boolean minus = false;
+        int c, n = System.in.read();
+        if (n == 45) {
+            minus = true;
+            n = System.in.read();
         }
-        quickSort(arr, 0, n - 1);
-        
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
-            if (check(arr, Integer.parseInt(st.nextToken()))) bw.write("1\n");
-            else bw.write("0\n");
-        }
-        bw.flush();
+        n = n & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return minus? -n: n;
     }
 
-    public static boolean check(int[] arr, int num) {
-        int l = 0, r = arr.length - 1;
+    public static void main(String[] args) throws Exception {
+        N = read();
+        sort = new int[N];
+        for (int i = 0; i < N; i++) {
+            sort[i] = read();
+        }
+        M = read();
+        arr = new int[M];
+        for (int i = 0; i < M; i++) {
+            arr[i] = read();
+        }
+        Arrays.sort(sort);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            sb.append(binarySearch(i)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static int binarySearch(int id) {
+        int l = 0, r = N - 1;
         while (l <= r) {
-            int m = (l + r) / 2;
-            if (num == arr[m]) return true;
-            else if (num < arr[m]) r = m - 1;
-            else if (num > arr[m]) l = m + 1;
+            int mid = (l + r) / 2;
+            if (arr[id] < sort[mid]) r = mid - 1;
+            else if (arr[id] > sort[mid]) l = mid + 1;
+            else return 1;
         }
-        return false;
-    }
-
-    public static void quickSort(int[] arr, int l, int r) {
-        int part = partition(arr, l, r);
-        if (l < part - 1) quickSort(arr, l, part - 1);
-        if (r > part) quickSort(arr, part, r);
-    }
-
-    public static int partition(int[] arr, int l, int r) {
-        int m = (l + r) / 2;
-        int pivot = arr[m];
-
-        while (l <= r) {
-            while (arr[l] < pivot) l++;
-            while (arr[r] > pivot) r--;
-            if (l <= r) {
-                int tmp = arr[l];
-                arr[l] = arr[r];
-                arr[r] = tmp;
-                l++;
-                r--;
-            }
-        }
-        return l;
+        return 0;
     }
 }
