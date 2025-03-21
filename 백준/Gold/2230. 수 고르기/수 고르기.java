3b892947f4992da7e08230794076a164af8bb912
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-22 04:55:29 +0900

    [Gold V] Title: 수 고르기, Time: 244 ms, Memory: 24108 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.java
+++ 백준/Gold/2230. 수 고르기/수 고르기.java
@@ -1,14 +1,23 @@
-import java.io.*;
 import java.util.*;
 
 class Main {
+    static int read() throws Exception {
+        boolean minus = false;
+        int c, n = System.in.read();
+        if (n == 45) {
+            minus = true;
+            n = System.in.read();
+        }
+        n = n & 15;
+        while ((c = System.in.read()) > 47) n = (n << 3) + (n << 1) + (c & 15);
+        return minus? -n: n;
+    }
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        int N = Integer.parseInt(split[0]), M = Integer.parseInt(split[1]);
+        int N = read(), M = read();
         int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = read();
         }
 
         Arrays.sort(arr);
