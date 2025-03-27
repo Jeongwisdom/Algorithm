16eeef86a895e4eff36aadd2af631dee3d32ac78
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-03-28 00:45:37 +0900

    [Silver I] Title: 영역 구하기, Time: 104 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,58 +1,59 @@
 import java.util.*;
-import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        boolean[][] arr = new boolean[n][m];
-        for (int i = 0; i < k; i++) {
-            st = new StringTokenizer(br.readLine());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            for (int a = x1; a < x2; a++) {
-                for (int b = y1; b < y2; b++) {
-                    arr[a][b] = true;
+    static int n, m;
+    static int[][] arr;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+
+    static int read() throws Exception {
+        int r, c = System.in.read() & 15;
+        while ((r = System.in.read()) > 47) c = (c << 3) + (c << 1) + (r & 15);
+        return c;
+    }
+
+    public static void main(String[] args) throws Exception {
+        m = read();
+        n = read();
+        int k = read();
+        arr = new int[n][m];
+        while (k-- > 0) {
+            int x1 = read(), y1 = read(), x2 = read() , y2 = read();
+            for (int i = x1; i < x2; i++) {
+                for (int j = y1; j < y2; j++) {
+                    arr[i][j] = 1;
                 }
             }
         }
-        boolean[][] ch = new boolean[n][m];
-        Queue<int[]> q = new ArrayDeque<>();
-        Queue<Integer> pq = new PriorityQueue<>();
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!arr[i][j] && !ch[i][j]) {
-                    ch[i][j] = true;
-                    q.offer(new int[] {i, j});
-                    int area = 0;
-                    while (!q.isEmpty()) {
-                        int[] point = q.poll();
-                        for (int l = 0; l < 4; l++) {
-                            int nx = point[0] + dx[l];
-                            int ny = point[1] + dy[l];
-                            if (nx >= 0 && ny >= 0 && nx < n && ny < m && !arr[nx][ny] &&!ch[nx][ny]) {
-                                ch[nx][ny] = true;
-                                q.offer(new int[] {nx, ny});
-                            }
-                        }
-                        area++;
-                    }
-                    pq.offer(area);
-                }
+                if (arr[i][j] == 1) continue;
+                arr[i][j] = 1;
+                list.add(DFS(i, j));
             }
         }
+
+        Collections.sort(list);
         StringBuilder sb = new StringBuilder();
-        sb.append(pq.size()).append("\n");
-        while (!pq.isEmpty()) {
-            sb.append(pq.poll()).append(" ");
+        sb.append(list.size()).append("\n");
+        for (int num: list) {
+            sb.append(num).append(' ');
         }
         System.out.println(sb);
+    }
+
+    static int DFS(int x, int y) {
+        int cnt = 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (arr[nx][ny] == 1) continue;
+            arr[nx][ny] = 1;
+            cnt += DFS(nx, ny);
+        }
+        return cnt;
     }
 }
