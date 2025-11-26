e63b2d108f5d94e00eefa2fe3f39cb5825134c09
Jeongwisdom <108439935+Jeongwisdom@users.noreply.github.com>
2025-11-26 11:44:27 +0900

    [Bronze V] Title: Bottle Return, Time: 100 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,15 @@
+class Main {
+    static int read() throws Exception {
+        int c, n = System.in.read() & 15;
+        while ((c = System.in.read()) > 47) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int num = read() + read() + read() + read() + read() + read();
+        num *= 5;
+        System.out.println(num);
+    }
+}
